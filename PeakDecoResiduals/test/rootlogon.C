0de0432c39efc1b992a0789d1088c52d7a4f9cc7
benhoob <>
2010-05-15 11:43:28 +0000

    *** empty log message ***

---
+++ PeakDecoResiduals/test/rootlogon.C
@@ -0,0 +1,10 @@
+{
+cout<<"Ben's rootlogon.C"<<endl;
+gSystem->Load("libGraf");
+gSystem->Load("libGpad");
+gSystem->Load("libTree");
+
+gROOT->ProcessLine(".L /tas03/home/benhoob/benstyle.C");
+setBenStyle();
+//gROOT->SetStyle("Plain");
+}
