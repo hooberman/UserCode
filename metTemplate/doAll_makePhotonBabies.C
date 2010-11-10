94e75b5e8063d35a47f23d657ce2fdf2bb2bb4c2
benhoob <>
2010-11-10 15:56:56 +0000

    Initial commit of code to make photon babies

---
+++ metTemplate/doAll_makePhotonBabies.C
@@ -0,0 +1,20 @@
+{
+
+#include "makePhotonBabies.h"
+  
+  gROOT->ProcessLine(".L histtools.C+");
+  gROOT->ProcessLine(".L CORE/CMS2.cc+");
+  gROOT->ProcessLine(".L CORE/trackSelections.cc+");
+  gROOT->ProcessLine(".L CORE/metSelections.cc+");
+  gROOT->ProcessLine(".L CORE/eventSelections.cc+");
+  gROOT->ProcessLine(".L CORE/electronSelectionsParameters.cc+");
+  gROOT->ProcessLine(".L CORE/electronSelections.cc+");
+  gROOT->ProcessLine(".L CORE/muonSelections.cc+");
+  gSystem->Load("Tools/MiniFWLite/libMiniFWLite.so");
+  gROOT->ProcessLine(".L runMakePhotonBabies.C+");
+  
+  //--photon+jets---
+  //runMakePhotonBabies("PhotonJet"             , false );   //PhotonJet MC
+  //runMakePhotonBabies("EG"                    , true  );   //EG data
+  runMakePhotonBabies("Photon"                , true  );   //Photon data
+}
