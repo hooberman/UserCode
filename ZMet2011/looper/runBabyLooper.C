afc35e401b7c1cb0a595bd77c2b6c349469d4d56
benhoob <>
2011-04-03 19:39:04 +0000

    Code based on tas metTemplate dir

---
+++ ZMet2011/looper/runBabyLooper.C
@@ -0,0 +1,21 @@
+#include "TChain.h"
+#include "babylooper.C"
+
+void runBabyLooper(char* Z_version, char* template_version, char* prefix , bool isData = true, 
+                   babylooper::selectionType sel = babylooper::e_QCDSelection, 
+                   bool makeTemplate = false){
+
+  TChain* ch = new TChain("T1");
+
+  string file = Form("../output/%s/%s_baby.root",Z_version,prefix);
+  cout << "Adding " << file << endl;
+
+  ch->Add( file.c_str() );
+
+  babylooper* myLooper = new babylooper();
+  
+  cout << "Running on sample " << prefix << endl;
+  myLooper->ScanChain(ch, Z_version, template_version, prefix, isData, sel, makeTemplate);
+  
+}
+
