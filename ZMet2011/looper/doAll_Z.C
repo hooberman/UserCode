a10528279216c6b5179ce59d1fde894e7eaa4d4f
benhoob <>
2011-05-03 10:18:02 +0000

    Updates

--- ZMet2011/looper/doAll_Z.C
+++ ZMet2011/looper/doAll_Z.C
@@ -2,11 +2,11 @@
   gROOT->ProcessLine(".L histtools.C+");
   gROOT->ProcessLine(".L ../CORE/CMS2.cc+");
   gROOT->ProcessLine(".L ../CORE/trackSelections.cc+");
-  gROOT->ProcessLine(".L ../CORE/metSelections.cc+");
   gROOT->ProcessLine(".L ../CORE/MITConversionUtilities.cc+");
   gROOT->ProcessLine(".L ../CORE/eventSelections.cc+");
   gROOT->ProcessLine(".L ../CORE/electronSelectionsParameters.cc+");
   gROOT->ProcessLine(".L ../CORE/electronSelections.cc+");
+  gROOT->ProcessLine(".L ../CORE/metSelections.cc+");
   gROOT->ProcessLine(".L ../CORE/muonSelections.cc+");
   gROOT->ProcessLine(".L ../CORE/utilities.cc+");
   gROOT->ProcessLine(".L ../CORE/triggerUtils.cc+");
@@ -14,7 +14,8 @@
   gROOT->ProcessLine(".L ../CORE/susySelections.cc+");
   gROOT->ProcessLine(".L ../CORE/jetSelections.cc+");
   gROOT->ProcessLine(".L ../CORE/mcSUSYkfactor.cc+");
-  
+  gSystem->Load("../Tools/MiniFWLite/libMiniFWLite.so");
+
   gROOT->ProcessLine(".L runZLooper.C+");
 
   runZLooper("data"           , true );
