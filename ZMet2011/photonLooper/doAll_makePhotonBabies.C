4a6d946fa51fb75eb95cabbb6eae49a8a098b2ed
benhoob <>
2011-05-06 14:29:07 +0000

    Minor updates

--- ZMet2011/photonLooper/doAll_makePhotonBabies.C
+++ ZMet2011/photonLooper/doAll_makePhotonBabies.C
@@ -20,7 +20,5 @@
   gROOT->ProcessLine(".L runMakePhotonBabies.C+");
   
   //--photon+jets---
-  //runMakePhotonBabies("PhotonJet"             , false );   //PhotonJet MC
-  //runMakePhotonBabies("EG"                    , true  );   //EG data
   runMakePhotonBabies("Photon"                , true  );   //Photon data
 }
