b7d02589849df8862f5e5858e3fd842f321493ac
benhoob <>
2011-06-03 08:49:49 +0000

    Update before altering

--- OSSusy2011/scripts/victory.C
+++ OSSusy2011/scripts/victory.C
@@ -1,5 +1,6 @@
 #include "TROOT.h"
 #include "TH1.h"
+#include "TH2.h"
 #include "TFile.h"
 #include "TGraph.h"
 #include "TCanvas.h"
@@ -11,6 +12,7 @@
 #include <string>
 #include <sstream>
 #include "TLegendEntry.h"
+#include "TGraphErrors.h"
 #include "TTree.h"
 #include "TChain.h"
 #include "TLatex.h"
@@ -24,15 +26,19 @@ using namespace std;
 //const float scale_factor = 2;
 const int   width1      = 20;
 const int   width2      = 4;
-const char* iter        = "../output/V00-00-05/highpt/";
+//const char* iter        = "../output/V00-00-10/highpt/";
+const char* iter        = "../output/V00-01-00/highpt/";
 const bool  plotData    = true;
 const int   nprec       = 2;
 const char* data        = "data";
-const bool  issignal    = true; //signal/control?
+const bool  issignal    = false; //signal/control?
+
+const char* metvar   = "y";
+const char* dilptvar = "dilpt/sqrt(htpf)";
 
 //-------------------------------------------------------
 
-const bool  makeLatexPlot = false;        //plot in latex style
+const bool  makeLatexPlot = true;        //plot in latex style
 char* pm         = " +/- ";
 char* delim      = "|";
 char* delimstart = "|";
@@ -43,7 +49,7 @@ char* em         = "em";
 
 string plottitle  = "control region (125 < H_{T} < 300 GeV)";
 
-float getMCratio( TH1F* hobs , TH1F* hpred , float cut );
+pair<float,float> getMCratio( TH1F* hobs , TH1F* hpred , float cut );
 void plotHist( TH1F* hpred , TH1F* hobs , TH1F* hpred_data , TH1F* hobs_data,
                string title, string xtitle , int rebin, float metcut);
 
@@ -57,7 +63,7 @@ void printLine(){
     cout << "\\hline" << endl;
   }
   else{
-    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "-------------------------------------------------------------------------------------------------" << endl;
         
   }
 }
@@ -70,9 +76,104 @@ void printHeader(){
        << delimend   << endl;
 }
 
-void victory( bool printgif =false ){
 
-  if( issignal ) plottitle  = "signal region (H_{T} > 300 GeV)";
+void deleteHistos() {
+   // Delete all existing histograms in memory
+   TObject* obj;
+   TList* list = gDirectory->GetList() ;
+   TIterator* myiter = list->MakeIterator();
+   while ((obj=myiter->Next())) {
+     if (obj->IsA()->InheritsFrom(TH1::Class()) ||
+         obj->IsA()->InheritsFrom(TH2::Class()) ) {delete obj;}
+   }
+}
+
+
+pair<float,float> victory( float ycut = 8.5 , float htcut = 300.0 , bool printgif =false );
+
+void run(){
+
+  /*
+  pair<float,float> mcratio_pair = victory();
+  float mcratio    = mcratio_pair.first;
+  float mcratioerr = mcratio_pair.second;
+
+  cout << "MC ratio " << mcratio << " +/- " << mcratioerr << endl;  
+  */
+
+  //bool y_vary = true;
+  //const unsigned int n = 16;
+
+  bool y_vary = false;
+  const unsigned int n = 10;
+
+  float ycut  = 8.5;
+  //float ycut  = 13.0;
+  float htcut = 300.0;
+
+
+  float x[n];
+  float xerr[n];
+  float op[n];
+  float operr[n];
+
+  for( unsigned int i = 0 ; i < n ; ++i ){
+
+    deleteHistos();
+
+    if( y_vary ){
+      ycut = 6.5 + i * 0.5;
+      x[i]    = ycut;
+      xerr[i] = 0.0;
+    }
+
+    else{
+      htcut = 200 + 50 * i;
+      x[i]    = htcut;
+      xerr[i] = 0.0;
+    }
+
+    pair<float,float> mcratio_pair = victory( ycut , htcut , false );
+    float mcratio    = mcratio_pair.first;
+    float mcratioerr = mcratio_pair.second;
+
+    op[i]    = mcratio;
+    operr[i] = mcratioerr;
+
+    cout << "y > " << ycut << " ht > " << htcut << " O/P " << Form("%.2f +/- %.2f",op[i],operr[i]) << endl;
+
+  }
+
+
+
+  TGraphErrors *gr  = new TGraphErrors(n,x,op ,xerr,operr);
+
+  TCanvas *c1 = new TCanvas();
+  c1->cd();
+
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+
+  gr->SetLineColor(2);
+  gr->SetMarkerColor(2);
+  gr->SetMarkerStyle(20);
+
+  if( y_vary ) gr->GetXaxis()->SetTitle("y cut (GeV^{1/2})");
+  else         gr->GetXaxis()->SetTitle("H_{T} cut (GeV)");
+  gr->GetYaxis()->SetTitle("observed / predicted");
+  gr->Draw("AP");
+
+}
+
+
+pair<float,float> victory( float ycut  , float htcut  , bool printgif  ){
+
+  cout << "y > " << ycut << " ht > " << htcut << endl;
+
+
+  deleteHistos();
+
+  if( issignal ) plottitle  = Form("signal region (H_{T} > %.0f GeV)",htcut);
 
   if( makeLatexPlot ){
     pm         = " $\\pm$ ";
@@ -88,29 +189,31 @@ void victory( bool printgif =false ){
   float xmin = 0;
   float xmax = 21.25;
   
-  //int nbins  = 40;
+  //int nbins  = 400;
   //float xmin = 0;
   //float xmax = 20.;
 
   //declare MC samples to include
   vector <string> samples;
-  samples.push_back("ttall");
-  //samples.push_back("ttpowheg");
+  //samples.push_back("ttall");
+  samples.push_back("ttpowheg");
   //samples.push_back("ttdil");
   //samples.push_back("ttotr");
-  //samples.push_back("DYtot"); 
-  //samples.push_back("wjets");
-  //samples.push_back("ww");
-  //samples.push_back("wz");
-  //samples.push_back("zz");
-  //samples.push_back("tW");
+  // samples.push_back("DYtot"); 
+  // samples.push_back("wjets");
+  // samples.push_back("ww");
+  // samples.push_back("wz");
+  // samples.push_back("zz");
+  // samples.push_back("tW");
   //samples.push_back("LM1");
+
 
   //samples.push_back("DYee");
   //samples.push_back("DYmm");
   //samples.push_back("DYtautau");
 
   const unsigned int nsamples = samples.size();
+
 
   //-------------------------------
   //create MC chains and histos
@@ -134,7 +237,9 @@ void victory( bool printgif =false ){
     ch[i] = new TChain("t");
     ch[i]->Add(Form( "%s/%s_smallTree.root" , iter , samples.at(i).c_str() ));
     ch_mctot->Add(Form( "%s/%s_smallTree.root" , iter , samples.at(i).c_str() ));
- 
+
+    //cout << Form( "%s/%s_smallTree.root" , iter , samples.at(i).c_str()) << endl;
+
     hmety[i]    =  new TH1F(Form("%s_hmety"    , samples.at(i).c_str()) , 
                             Form("%s_hmety"    , samples.at(i).c_str()) , nbins,xmin,xmax);
 
@@ -148,7 +253,7 @@ void victory( bool printgif =false ){
     hmety[i]->Sumw2();
     hdilpt[i]->Sumw2();
   }
-  
+
   //create data chain and histos
   TChain *chdata = new TChain("t");
   if( plotData )
@@ -162,36 +267,51 @@ void victory( bool printgif =false ){
   hmety_data->Sumw2();
 
   //declare cuts for event selection
-  char* jetcutstring = "htpf>125&&htpf<300&&npfjets>1";
-  if( issignal ) jetcutstring = "htpf>300&&npfjets>1";
+  char* jetcutstring = Form("htpf>125&&htpf<%.0f&&npfjets>1",htcut);
+  if( issignal ) jetcutstring = Form("htpf>%.0f&&npfjets>1",htcut);
   TCut jetcut(jetcutstring);
   TCut zcut("passz==0");  
   TCut metcut("pfmet>50");
+  //TCut weight("weight * ndavtxweight");
   TCut weight("weight");
+  //TCut weight("weight * (1000./204.)");
+  //TCut weight("1");
 
-  TCut fcut  = ( jetcut + zcut +metcut ) * weight;
+  //declare cuts for event selection
+  //char* jetcutstring = "sumjetpt>125&&sumjetpt<300&&njets>1";
+  //if( issignal ) jetcutstring = "sumjetpt>300&&njets>1";
+  //TCut jetcut(jetcutstring);
+  //TCut zcut("passz==0");  
+  //TCut metcut("tcmet>50");
+  //TCut weight("weight");
 
+  TCut fcut       = ( jetcut + zcut + metcut ) * weight;
+  TCut fcut_data  = ( jetcut + zcut + metcut );
+
+  cout << "-------------------------------------------------------" << endl;
   cout << "Using selection: " << fcut.GetTitle() << endl;
+  cout << "-------------------------------------------------------" << endl;
+
 
   TCanvas *ctemp =new TCanvas();
   ctemp->cd();
 
   //total MC
-  ch_mctot->Draw("TMath::Min(tcmet/sqrt(sumjetpt),19.9)>>hmety_mctot",     fcut);
-  ch_mctot->Draw("TMath::Min(dilpt/sqrt(sumjetpt),19.9)>>hdilpty_mctot",   fcut);
-  ch_mctot->Draw("TMath::Min(dilpt,299.9)>>hdilpt_mctot",                  fcut);
+  //ch_mctot->Draw(Form("TMath::Min(%s,19.9)>>hmety_mctot"   , metvar   ) ,  fcut );
+  //ch_mctot->Draw(Form("TMath::Min(%s,19.9)>>hdilpty_mctot" , dilptvar ) ,  fcut );
+  ch_mctot->Draw("TMath::Min(dilpt,299.9)>>hdilpt_mctot"                ,  fcut );
 
   //apply dilepton pt scale factor
+  //float k = hdilpt_mctot->Integral() /  hdilpt_mctot->Integral( hdilpt_mctot->FindBin(50) , 100000 );
   float k = hdilpt_mctot->Integral() /  hdilpt_mctot->Integral( hdilpt_mctot->FindBin(50) , 100000 );
-  cout << "Scaling factor " << k << endl;
+  cout << "Scaling factor " << k << " " << hdilpt_mctot->Integral() << " / " << hdilpt_mctot->Integral( hdilpt_mctot->FindBin(50) , 100000 ) << endl;
   hdilpty_mctot->Scale( k );
 
-  
   //data
   if( plotData ){
-    chdata->Draw("TMath::Min(tcmet/sqrt(sumjetpt),19.9)>>hmety_data",     fcut);
-    chdata->Draw("TMath::Min(dilpt/sqrt(sumjetpt),19.9)>>hdilpty_data",   fcut);
-    chdata->Draw("TMath::Min(dilpt,299.9)>>hdilpt_data",                  fcut);
+    chdata->Draw(Form("TMath::Min(%s,19.9)>>hmety_data"   , metvar   ) ,  fcut_data );
+    chdata->Draw(Form("TMath::Min(%s,19.9)>>hdilpty_data" , dilptvar ) ,  fcut_data );
+    chdata->Draw("TMath::Min(dilpt,299.9)>>hdilpt_data",                  fcut_data );
   }
   delete ctemp;
 
@@ -207,15 +327,22 @@ void victory( bool printgif =false ){
   TH1F* hmety_temp     = new TH1F("hmety_temp"    , "" , nbins,xmin,xmax);     
   TH1F* hdilpty_temp   = new TH1F("hdilpty_temp"  , "" , nbins,xmin,xmax);     
   TH1F* hdilpt_temp    = new TH1F("hdilpt_temp"   , "" , 300 , 0 , 300);     
-  
+
   hmety_temp->Sumw2();
   hdilpty_temp->Sumw2();
 
+  TCut fcut_MC  = ( jetcut + zcut + metcut );
+
+  TCut fcut_MC_clone = fcut_MC;
+
   for( unsigned int i = 0 ; i < nsamples ; ++i ){
 
-    ch[i]->Draw("TMath::Min(tcmet/sqrt(sumjetpt),19.9)>>hmety_temp",     fcut);
-    ch[i]->Draw("TMath::Min(dilpt/sqrt(sumjetpt),19.9)>>hdilpty_temp",   fcut);
-    ch[i]->Draw("TMath::Min(dilpt,299.9)>>hdilpt_temp",                  fcut);
+    fcut_MC = fcut_MC_clone;
+    if( samples[i] == "DYtot" ) fcut_MC = fcut_MC + "ntaus==2";
+
+    ch[i]->Draw(Form("TMath::Min(%s,19.9)>>hmety_temp"    , metvar   )  , fcut_MC * weight );
+    ch[i]->Draw(Form("TMath::Min(%s,19.9)>>hdilpty_temp"  , dilptvar )  , fcut_MC * weight );
+    ch[i]->Draw("TMath::Min(dilpt,299.9)>>hdilpt_temp",                   fcut_MC * weight );
 
     hmety[i]   = (TH1F*) hmety_temp->Clone(   Form("%s_hmety"    , samples.at(i).c_str()) );
     hdilpty[i] = (TH1F*) hdilpty_temp->Clone( Form("%s_hdilpty"  , samples.at(i).c_str()) );
@@ -225,30 +352,10 @@ void victory( bool printgif =false ){
     hmety_temp->Reset();
     hdilpty_temp->Reset();
 
-  }
-  
-  /*
-  TH1F *hmety_mcsum   =  (TH1F*) hmety[0]->Clone();
-  TH1F *hdilpty_mcsum =  (TH1F*) hdilpty[0]->Clone();
-  TH1F *hdilpt_mcsum  =  (TH1F*) hdilpt[0]->Clone();
-  
-  for( unsigned int i = 1 ; i < nsamples ; ++i ){
-
-    hmety[i]->Scale( scale_factor );
-    hdilpty[i]->Scale( scale_factor );
-    hdilpt[i]->Scale( scale_factor );
-    
-    hmety_mcsum->Add( hmety[i] );
-    hdilpty_mcsum->Add( hdilpty[i] );
-    hdilpt_mcsum->Add( hdilpt[i] );
-
+    hmety_mctot->Add(hmety[i]);
+    hdilpty_mctot->Add(hdilpty[i]);
   }
 
-  float knew = hdilpt_mcsum->Integral() /  hdilpt_mcsum->Integral( hdilpt_mcsum->FindBin(50) , 100000 );
-  cout << "knew " << knew << endl;
-  hdilpty_mcsum->Scale(knew);
-  printRow( "mcsum" , hmety_mcsum , hdilpty_mcsum , 8.5 );
-  */
 
   //dineutrino pt
   TCanvas *c1=new TCanvas("c1","",800,600);
@@ -260,7 +367,7 @@ void victory( bool printgif =false ){
 
 
   plotHist( hdilpty_mctot , hmety_mctot , hdilpty_data , hmety_data , 
-            plottitle , "y   (GeV^{1/2})" , 4 , 8.5 );
+            plottitle , "y   (GeV^{1/2})" , 4 , ycut );
 
   //if( printgif ) c1->Print("plots/victory.gif");
 
@@ -274,7 +381,7 @@ void victory( bool printgif =false ){
   title->SetTextSize(0.5);
   title->SetNDC();
   title->DrawLatex(0.18,0.15,"CMS");
-  title->DrawLatex(0.58,0.15,"L_{int} = 90 pb^{-1},  #sqrt{s} = 7 TeV");
+  title->DrawLatex(0.58,0.15,"L_{int} = 191 pb^{-1},  #sqrt{s} = 7 TeV");
 
   cout << endl << endl;
 
@@ -283,25 +390,26 @@ void victory( bool printgif =false ){
   printLine();
 
   for( unsigned int i = 0 ; i < nsamples ; ++i ){
-    printRow( samples.at(i) , hmety[i] , hdilpty[i] , 8.5 );
+    printRow( samples.at(i) , hmety[i] , hdilpty[i] , ycut );
   }
 
+
   printLine();
-  printRow( "total MC" , hmety_mctot , hdilpty_mctot , 8.5 );
+  printRow( "total MC" , hmety_mctot , hdilpty_mctot , ycut );
   printLine();
 
   //------------------------------
   // scale data by MC obs/pred
   //------------------------------
 
-  float mcratio = getMCratio(  hmety_mctot , hdilpty_mctot , 8.5 );  
+  pair<float,float> mcratio_pair = getMCratio(  hmety_mctot , hdilpty_mctot , ycut );  
+  float mcratio    = mcratio_pair.first;
+  float mcratioerr = mcratio_pair.second;
 
   if( plotData ){
-    printRow( "data" , hmety_data , hdilpty_data , 8.5 , mcratio );
+    printRow( "data" , hmety_data , hdilpty_data , ycut , mcratio );
     printLine();
   }
-
-
 
   if( printgif ){
     
@@ -316,85 +424,7 @@ void victory( bool printgif =false ){
   }
 
 
-  /*
-
-  //TCanvas *c1=new TCanvas("c1","",800,1200);
-  //c1->Divide(1,2);
-  //c1->cd(1);
-
-  cout << hmety->GetBinContent(17) << " " << hmety->GetBinError(17) << endl;
-  cout << hdilpty->GetBinContent(17) << " " << hdilpty->GetBinError(17) << endl;
-  cout << hmety->GetBinContent(17) / hdilpty->GetBinContent(17) << endl;
-
-  //TLatex t;
-  //t.SetNDC();
-  //t.DrawLatex(0.2,0.2,Form("k = %.3f",k));
-
-  //TCanvas *c2 = new TCanvas("c2","",800,600);
-  //c2->cd();
-  c1->cd(2);
-
-  TH1F* hratio = (TH1F*) hmety->Clone("ratio");
-  hratio->Divide( hdilpty );
-  //hratio->Rebin(2);
-  //hratio->Scale(0.5);
-
-  hratio->Draw();
-  hratio->SetMinimum(-1);
-  hratio->SetMaximum(5);
-  hratio->GetXaxis()->SetTitle("tcmet / #sqrt{sumJetPt} (GeV^{1/2})");
-  hratio->GetYaxis()->SetTitle("Observed / Predicted");
-  cout << hratio->GetBinContent(17) << " " << hratio->GetBinError(17) << endl;
-
-  TLine line;
-  line.SetLineStyle(2);
-  line.DrawLine(0,1.38,21.25,1.38);
-  line.DrawLine(8.5,-1,8.5,5);
-  */
-
-
-
-
-  //t->Draw("TMath::Min(nupt ,299)>>hnupt_jets","sumjetpt>200 && njets>1");
-  //t->Draw("TMath::Min(dilpt,299)>>hdilpt_jets","sumjetpt>200 && njets>1");
-  //t->Draw("TMath::Min(dilpt,299)>>hdilpt_nupt50","nupt>50");
-
-  /*
-  //dineutrino pt (nupt > 50 GeV)
-  float integral   =  hdilpt_nupt50->Integral();
-  float integral50 =  hdilpt_nupt50->Integral( hdilpt_nupt50->FindBin(50) , 100000 );
-  float k          =  integral / integral50;
-  cout << "Scaling factor " << k << endl;
-  hdilpt_nupt50->Scale( k );
-
-  TCanvas *c2=new TCanvas("c2","",800,600);
-  c2->cd();
-  plotHist( hdilpt_nupt50 , hnupt , "" , "neutrino p_{T} (GeV)" , 1);
-
-  //dineutrino pt (with jet cuts)
-  TCanvas *c3=new TCanvas("c3","",800,600);
-  c3->cd();
-  plotHist(  hdilpt_jets , hnupt_jets , "sumJetPt > 200 GeV, nJets #geq 2" , "neutrino p_{T} (GeV)" , 1);
-  */
-
-
-  
-  //ee 
-  //t->Draw("TMath::Min(tcmet,299)>>hmet","(sumjetpt>200&&passz==0&&njets>1&&leptype==0)");
-  //t->Draw("TMath::Min(dilpt,299)>>hdilpt","(tcmet>50&&sumjetpt>200&&passz==0&&njets>1&&leptype==0)");
-
-  //mm
-  //t->Draw("TMath::Min(tcmet,299)>>hmet","(sumjetpt>200&&passz==0&&njets>1&&leptype==1)");
-  //t->Draw("TMath::Min(dilpt,299)>>hdilpt","(tcmet>50&&sumjetpt>200&&passz==0&&njets>1&&leptype==1)");
-
-  //em
-  //t->Draw("TMath::Min(tcmet,299)>>hmet","(sumjetpt>200&&passz==0&&njets>1&&leptype==2)");
-  //t->Draw("TMath::Min(dilpt,299)>>hdilpt","(tcmet>50&&sumjetpt>200&&passz==0&&njets>1&&leptype==2)");
-
-
-
-  //float integral   =  hmet->Integral();
-  //float integral50 =  hmet->Integral( hdilpt->FindBin(50) , 100000 );
+  return make_pair( mcratio , mcratioerr );
 
  
 }
@@ -411,16 +441,21 @@ float calculateHistError( TH1F* h , int minbin , int maxbin ){
 }
 
 
-float getMCratio( TH1F* hobs , TH1F* hpred , float cut ){
+pair<float,float> getMCratio( TH1F* hobs , TH1F* hpred , float cut ){
 
   int   minbin     = hobs->FindBin( cut );
   int   maxbin     = 10000;
 
   float pred    = hpred->Integral( minbin , maxbin);
+  float prederr = calculateHistError( hpred , minbin , maxbin );
+
   float obs     = hobs->Integral(  minbin , maxbin );
+  float obserr  = calculateHistError( hobs , minbin , maxbin );
+
   float ratio   = pred > 0 ? obs / pred : 0;
- 
-  return ratio;
+  float ratioerr = obs > 0 && pred > 0 ? ratio * sqrt( pow( prederr / pred , 2 ) + pow( obserr / obs , 2 ) ) : 0;
+
+  return make_pair( ratio , ratioerr );
 }
 
 void printRow( string sample , TH1F* hobs , TH1F* hpred , float cut , float mcratio ){
@@ -448,8 +483,8 @@ void printRow( string sample , TH1F* hobs , TH1F* hpred , float cut , float mcra
   float ratio    = pred > 0 ? obs / pred : 0;
   float ratioerr = obs > 0 && pred > 0 ? ratio * sqrt( pow( prederr / pred , 2 ) + pow( obserr / obs , 2 ) ) : 0;
   
-  sobs   << Form( "%.2f" , obs   ) << pm << Form( "%.2f" , obserr   ); 
-  spred  << Form( "%.2f" , pred  ) << pm << Form( "%.2f" , prederr  ); 
+  sobs   << Form( "%.1f" , obs   ) << pm << Form( "%.1f" , obserr   ); 
+  spred  << Form( "%.1f" , pred  ) << pm << Form( "%.1f" , prederr  ); 
   sratio << Form( "%.2f" , ratio ) << pm << Form( "%.2f" , ratioerr ); 
 
   cout << delimstart << setw(width1) << sample        << setw(width2)
@@ -491,8 +526,8 @@ void plotHist( TH1F* hpred , TH1F* hobs , TH1F* hpred_data, TH1F* hobs_data,
   hpred->GetYaxis()->SetTitleOffset(1);
   //hpred->GetYaxis()->SetTitle( "Entries / 2.125 GeV" );
 
-  hpred->SetMinimum(0.001);
-  hpred->SetMaximum(1000.);
+  hpred->SetMinimum(0.01);
+  hpred->SetMaximum(10000.);
 
   int metbin = hobs->FindBin( metcut );
   float pred = hpred->Integral( metbin , 100000000);
@@ -507,7 +542,7 @@ void plotHist( TH1F* hpred , TH1F* hobs , TH1F* hpred_data, TH1F* hobs_data,
 
   //int width1 = 10;
   //int width2 = 2;
-
+  /*
   cout << endl;
   cout << "|" << setw(width1) << ""          << setw(width2)
        << "|" << setw(width1) << "Predicted" << setw(width2)
@@ -526,7 +561,7 @@ void plotHist( TH1F* hpred , TH1F* hobs , TH1F* hpred_data, TH1F* hobs_data,
          << "|" << setw(width1) << fround(obs_data/pred_data,nprec)  << setw(width2) << "|" << endl;
   }
   cout << endl;
-
+  */
 
   stringstream spred;
   spred << "Pred: " << pred ;
@@ -573,7 +608,7 @@ void plotHist( TH1F* hpred , TH1F* hobs , TH1F* hpred_data, TH1F* hobs_data,
   TLine line;
   line.SetLineWidth(2);
   line.SetLineStyle(2);
-  line.DrawLine( metcut , 0.001 , metcut , 1000 );
+  line.DrawLine( metcut , 0.01 , metcut , 10000 );
   //line.DrawLine( metcut , 0.5 * TMath::Min( hobs->GetMinimum() , hpred->GetMinimum() ),
   //               metcut , 2 *   TMath::Max( hobs->GetMaximum() , hpred->GetMaximum() ) );
 
