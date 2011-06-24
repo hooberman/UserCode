48c989769866ee8bca65d79da5b170e16fcd8464
benhoob <>
2011-06-24 12:07:04 +0000

    Update json

--- OSTagAndProbe/doData.C
+++ OSTagAndProbe/doData.C
@@ -17,8 +17,8 @@
 
     chain_data2011_skim->Add("cms2/DoubleElectron_Run2011A-May10ReReco-v1_AOD/V04-02-15/tagAndProbeSkim/skim*root");
     chain_data2011_skim->Add("cms2/DoubleMu_Run2011A-May10ReReco-v1_AOD/V04-02-15/tagAndProbeSkim/skim*root");
-    chain_data2011_skim->Add("cms2/DoubleMu_Run2011A-PromptReco-v4_AOD/V04-02-16/tagAndProbeSkim/");
-    chain_data2011_skim->Add("cms2/DoubleElectron_Run2011A-PromptReco-v4_AOD/V04-02-16/tagAndProbeSkim/");
+    chain_data2011_skim->Add("cms2/DoubleMu_Run2011A-PromptReco-v4_AOD/V04-02-16/tagAndProbeSkim/skim*root");
+    chain_data2011_skim->Add("cms2/DoubleElectron_Run2011A-PromptReco-v4_AOD/V04-02-16/tagAndProbeSkim/skim*root");
     
     //TChain *chain_data2011_express = new TChain("Events");
     //chain_data2011_express->Add("cms2/ExpressPhysicsRun2011A-Express-v1FEVT/V04-00-08/ntuple*.root");
@@ -29,7 +29,7 @@
     //looper->setGoodRunList("runlists/Cert_TopOct15_Merged_135821-147454_allPVT.txt");
     // for 2011 unofficial so far
     //looper->setGoodRunList("runlists/json_DCSONLY_ManualCert.txt");
-    looper->setGoodRunList("Cert_160404-163757_7TeV_PromptReco_Collisions11_JSON_goodruns.txt");
+    looper->setGoodRunList("Cert_160404-163869_7TeV_May10ReReco_160404-166861_7TeV_PromptReco_Collisions11_JSON_goodruns.txt");
 
     // do the looping
     looper->ScanChain(true, "data_skim", chain_data2011_skim);
