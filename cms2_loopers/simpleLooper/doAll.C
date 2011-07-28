0a59746b002e947c96786553940a39c55571a245
benhoob <>
2011-07-28 11:25:18 +0000

    Fix compile

--- cms2_loopers/simpleLooper/doAll.C
+++ cms2_loopers/simpleLooper/doAll.C
@@ -1,14 +1,20 @@
 {
-
   gROOT->ProcessLine(".L CORE/CMS2.cc+");
+  gROOT->ProcessLine(".L CORE/utilities.cc+");
   gROOT->ProcessLine(".L CORE/trackSelections.cc+");
-  gROOT->ProcessLine(".L CORE/MITConversionUtilities.cc+");
   gROOT->ProcessLine(".L CORE/eventSelections.cc+");
+  gROOT->ProcessLine(".L CORE/MITConversionUtilities.cc+");
+  gROOT->ProcessLine(".L CORE/muonSelections.cc+");
   gROOT->ProcessLine(".L CORE/electronSelectionsParameters.cc+");
   gROOT->ProcessLine(".L CORE/electronSelections.cc+");
   gROOT->ProcessLine(".L CORE/metSelections.cc+");
-  gROOT->ProcessLine(".L CORE/muonSelections.cc+");
-  gROOT->ProcessLine(".L CORE/jetSelections.cc+");
+  gROOT->ProcessLine(".L CORE/SimpleFakeRate.cc+");
+  gROOT->ProcessLine(".L CORE/mcSelections.cc+");
+  gROOT->ProcessLine(".L CORE/MT2/MT2.cc+");
+  gROOT->ProcessLine(".L CORE/triggerUtils.cc+");  
+  gROOT->ProcessLine(".L CORE/susySelections.cc+");
+  gROOT->ProcessLine(".L CORE/mcSUSYkfactor.cc+");
+  gROOT->ProcessLine(".L CORE/triggerSuperModel.cc+");
   gROOT->ProcessLine(".L CORE/triggerUtils.cc+");
   gROOT->ProcessLine(".L CORE/ttbarSelections.cc+");
   gROOT->ProcessLine(".L histtools.C+");
