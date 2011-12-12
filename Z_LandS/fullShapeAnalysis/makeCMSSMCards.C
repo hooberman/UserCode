86e78a75c1e281fc6c3e96a5cad3e0f45d0a75e4
benhoob <>
2011-12-12 18:27:18 +0000

    3 jets, T5zz

--- Z_LandS/fullShapeAnalysis/makeCMSSMCards.C
+++ Z_LandS/fullShapeAnalysis/makeCMSSMCards.C
@@ -69,9 +69,9 @@ void makeCMSSMCards(){
   
   TChain *ch = new TChain("T1");
   ch->Add("output/V00-02-04/T5zz_baby.root");
-  char* version = "V00-01-00";
+  char* version = "V00-01-01";
 
-  bool do3jets = false;
+  bool do3jets = true;
 
   //---------------------------------------
   // selection
