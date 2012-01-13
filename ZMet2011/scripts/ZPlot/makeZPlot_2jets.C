2bd548d6783bd525d1a8a00fe508694f7fc42ed4
benhoob <>
2012-01-13 12:35:56 +0000

    style changes a la frederic

--- ZMet2011/scripts/ZPlot/makeZPlot_2jets.C
+++ ZMet2011/scripts/ZPlot/makeZPlot_2jets.C
@@ -5,6 +5,7 @@
 #include <fstream>
 
 #include "TCanvas.h"
+#include "TPaveText.h"
 #include "TLegend.h"
 #include "TChain.h"
 #include "TDirectory.h"
@@ -81,8 +82,28 @@ void makeZPlot_2jets( bool print = false ){
   // make the plot
   //------------------------------------------------------
 
-  TCanvas *can = new TCanvas();
-  doPlot( can , hist_VV , hist_OF , hist_photon , hist_QCD , hist_data , true , print );
+  //TCanvas *can = new TCanvas();
+  //doPlot( can , hist_VV , hist_OF , hist_photon , hist_QCD , hist_data , true , print );
+
+  TCanvas *main_canvas = new TCanvas("main_canvas", "main_canvas",0,0,600,750);
+  gStyle->SetOptFit(1);
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  main_canvas->SetHighLightColor(2);
+  main_canvas->Range(0,0,1,1);
+  main_canvas->SetFillColor(0);
+  main_canvas->SetBorderMode(0);
+  main_canvas->SetBorderSize(0);
+  main_canvas->SetTickx(1);
+  main_canvas->SetTicky(1);
+  main_canvas->SetLeftMargin(0.13);
+  main_canvas->SetRightMargin(0.05);
+  main_canvas->SetTopMargin(0.08);
+  main_canvas->SetBottomMargin(0.13);
+  main_canvas->SetFrameFillStyle(0);
+  main_canvas->SetFrameBorderMode(0);
+
+  doPlot( main_canvas , hist_VV , hist_OF , hist_photon , hist_QCD , hist_data , true , print );
 
 }
 
@@ -107,11 +128,30 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
   can->cd(); 
 
   if( residual ){
-    TPad* plotpad = new TPad("plotpad","plotpad",0.0,0.0,1.0,0.8);
+    // TPad* plotpad = new TPad("plotpad","plotpad",0.0,0.0,1.0,0.8);
+    // TPad* plotpad = new TPad("plotpad","plotpad",0.0,0.2,1.0,1.0);
+    // plotpad->Draw();
+    // plotpad->cd();
+    // plotpad->SetLogy();
+    // plotpad->SetRightMargin(0.05);
+
+    TPad *plotpad = new TPad("plotpad", "plotpad",0,0.2,1,1);
     plotpad->Draw();
     plotpad->cd();
     plotpad->SetLogy();
+    plotpad->Range(0,0,1,1);
+    plotpad->SetFillColor(0);
+    plotpad->SetBorderMode(0);
+    plotpad->SetBorderSize(0);
+    plotpad->SetTickx(1);
+    plotpad->SetTicky(1);
+    plotpad->SetLeftMargin(0.13);
     plotpad->SetRightMargin(0.05);
+    plotpad->SetTopMargin(0.08);
+    plotpad->SetBottomMargin(0.13);
+    plotpad->SetFrameFillStyle(0);
+    plotpad->SetFrameBorderMode(0);
+
   }else{
     gPad->SetLogy();
   }
@@ -120,15 +160,41 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
   // histogram formatting
   //-----------------------------------------
 
-  hist_data->SetLineColor(1);
-  hist_data->SetMarkerColor(1);
-  hist_data->SetMarkerSize(1);
-  hist_data->GetXaxis()->SetTitle("E_{T}^{miss} (GeV/c)");
-  hist_data->SetMinimum(0.05);
-  hist_data->Draw("E1");
   hist_data->SetMinimum(0.1);
-  hist_VV->SetFillColor(kViolet-1);
-  hist_OF->SetFillColor(kGreen-2);
+  //hist_data->SetMaximum(29035);
+  hist_data->SetLineStyle(0);
+  hist_data->SetMarkerStyle(20);
+  hist_data->GetXaxis()->SetTitle("E_{T}^{miss} [GeV]");
+  hist_data->GetXaxis()->CenterTitle(true);
+  hist_data->GetXaxis()->SetLabelFont(42);
+  hist_data->GetXaxis()->SetLabelOffset(0.007);
+  hist_data->GetXaxis()->SetTitleSize(0.05);
+  hist_data->GetXaxis()->SetTitleFont(42);
+  hist_data->GetYaxis()->SetTitle("events / 5 GeV");
+  hist_data->GetYaxis()->CenterTitle(true);
+  hist_data->GetYaxis()->SetLabelFont(42);
+  hist_data->GetYaxis()->SetLabelOffset(0.007);
+  hist_data->GetYaxis()->SetTitleSize(0.05);
+  hist_data->GetYaxis()->SetLabelSize(0.04);
+  hist_data->GetYaxis()->SetTitleOffset(1.2);
+  hist_data->GetYaxis()->SetTitleFont(42);
+  hist_data->GetZaxis()->SetLabelFont(42);
+  hist_data->GetZaxis()->SetLabelOffset(0.007);
+  hist_data->GetZaxis()->SetTitleSize(0.05);
+  hist_data->GetZaxis()->SetTitleFont(42);
+  hist_data->Draw("E1");
+
+  // hist_data->SetLineColor(1);
+  // hist_data->SetMarkerColor(1);
+  // hist_data->SetMarkerSize(1);
+  // hist_data->GetXaxis()->SetTitle("E_{T}^{miss} [GeV]");
+  // hist_data->GetYaxis()->SetTitle("events / 5 GeV");
+  // hist_data->SetMinimum(0.05);
+  // hist_data->Draw("E1");
+  // hist_data->SetMinimum(0.1);
+
+  hist_VV->SetFillColor(kGreen+2);
+  hist_OF->SetFillColor(kMagenta+2);
   //hist_photon->SetFillColor(kRed-4);
   hist_photon->SetLineColor(kRed);
   hist_QCD->SetLineColor(kBlue);
@@ -178,7 +244,7 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
   // make the legend
   //-----------------------------------------
 
-  TLegend *leg = new TLegend(0.55,0.45,0.9,0.7);
+  TLegend *leg = new TLegend(0.55,0.55,0.9,0.9);
   leg->AddEntry(hist_data,"data","lp");
   leg->AddEntry(hist_photon    ,"total bkg (#gamma+jets)","l");
   //leg->AddEntry(hist_QCD    ,"total bkg (QCD)","l");
@@ -194,21 +260,76 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
 
   TLatex *text = new TLatex();
   text->SetNDC();
-  text->SetTextSize(0.05);
-  text->DrawLatex(0.42,0.88,"CMS Preliminary");
-  text->DrawLatex(0.42,0.81,"#sqrt{s} = 7 TeV, #scale[0.6]{#int}L dt = 4.7 fb^{-1}");
-  text->DrawLatex(0.42,0.74,"Events with ee/#mu#mu + #geq2 jets");
+  text->SetTextSize(0.042);
+  //text->DrawLatex(0.42,0.88,"CMS Preliminary");
+  //text->DrawLatex(0.42,0.81,"#sqrt{s} = 7 TeV, #scale[0.6]{#int}L dt = 4.7 fb^{-1}");
+  text->DrawLatex(0.53,0.48,"Z#rightarrow ee/#mu#mu + #geq2 jets");
+
+  text->DrawLatex(0.14,0.95,"CMS Preliminary,   #sqrt{s} = 7 TeV,   L_{int} = 4.7 fb^{-1}");
+
+  TPaveText *pt = new TPaveText(0.27,0.93,0.77,1,"blNDC");
+  // pt->SetBorderSize(0);
+  // pt->SetFillColor(0);
+  // pt->SetFillStyle(4000);
+  // pt->SetTextFont(42);
+  // pt->SetTextSize(0.042);
+  
+  // TText *ttext = pt->AddText("CMS Preliminary, #sqrt{s} = 7 TeV, L_{int} = 4.7 fb^{-1}");
+  // pt->Draw("same");
 
   //-----------------------------------------
   // make a TPad for the ratio histogram
   //-----------------------------------------
 
   if( residual ){
+
+    //TPad* respad = new TPad("respad","respad",0.0,0.8,1.0,1.0);
+    
+    //plotpad->cd();
+    //plotpad->Modified();
+    can->cd();    
+
+   // ------------>Primitives in pad: coverpad
+    TPad* coverpad = new TPad("coverpad", "coverpad",0.122,0.16,1,0.303);
+    coverpad->Draw();
+    coverpad->cd();
+    coverpad->Range(0,0,1,1);
+    coverpad->SetFillColor(0);
+    coverpad->SetBorderMode(0);
+    coverpad->SetBorderSize(0);
+    coverpad->SetTickx(1);
+    coverpad->SetTicky(1);
+    coverpad->SetLeftMargin(0.13);
+    coverpad->SetRightMargin(0.05);
+    coverpad->SetTopMargin(0.08);
+    coverpad->SetBottomMargin(0.13);
+    coverpad->SetFrameFillStyle(0);
+    coverpad->SetFrameBorderMode(0);
+    coverpad->Modified();
     can->cd();
-    TPad* respad = new TPad("respad","respad",0.0,0.8,1.0,1.0);
-    respad->Draw();
-    respad->cd();
-    respad->SetRightMargin(0.05);
+
+    // TPad* respad = new TPad("respad","respad",0.0,0.0,1.0,0.289);
+    // respad->Draw();
+    // respad->cd();
+    // respad->SetRightMargin(0.05);
+
+    TPad* bottompad = new TPad("bottompad", "Ratio Pad",0,0,1,0.289);
+    bottompad->Draw();
+    bottompad->cd();
+    bottompad->Range(-63.41463,-1,424.3902,2.333333);
+    bottompad->SetFillColor(0);
+    bottompad->SetBorderMode(0);
+    bottompad->SetBorderSize(2);
+    bottompad->SetTickx(1);
+    bottompad->SetTicky(1);
+    bottompad->SetLeftMargin(0.13);
+    bottompad->SetRightMargin(0.05);
+    bottompad->SetBottomMargin(0.3);
+    bottompad->SetFrameFillStyle(0);
+    bottompad->SetFrameBorderMode(0);
+    bottompad->SetFrameFillStyle(0);
+    bottompad->SetFrameBorderMode(0);
+
 
     //-----------------------------------------
     // make the histogram of syst uncertainty
@@ -238,20 +359,43 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
     for( unsigned int ibin = 1 ; ibin <= totpred->GetXaxis()->GetNbins() ; ibin++ )
       totpred->SetBinError(ibin,0);
    
-    TH1F* ratio = (TH1F*) hist_data->Clone("ratio");
+    TH1F* ratio = (TH1F*) hist_data->Clone("data/pred");
     ratio->Divide(totpred);
 
     //-----------------------------------------
     // histogram formatting
     //-----------------------------------------
 
-    ratio->GetXaxis()->SetLabelSize(0);
-    ratio->GetYaxis()->SetLabelSize(0.2);
-    ratio->GetYaxis()->SetNdivisions(5);
-    ratio->GetYaxis()->SetTitleSize(0.24);
-    ratio->GetYaxis()->SetTitleOffset(0.25);
+    // ratio->GetXaxis()->SetLabelSize(0);
+    // ratio->GetYaxis()->SetLabelSize(0.2);
+    // ratio->GetYaxis()->SetNdivisions(5);
+    // ratio->GetYaxis()->SetTitleSize(0.24);
+    // ratio->GetYaxis()->SetTitleOffset(0.25);
+    // ratio->GetYaxis()->SetRangeUser(0,2);
+    // ratio->GetXaxis()->SetTitle("");
+
     ratio->GetYaxis()->SetRangeUser(0,2);
-    ratio->GetXaxis()->SetTitle("");
+    ratio->GetXaxis()->SetTitle("E_{T}^{miss} [GeV]");
+    ratio->GetXaxis()->CenterTitle(true);
+    ratio->GetXaxis()->SetLabelFont(42);
+    ratio->GetXaxis()->SetLabelOffset(0.007);
+    ratio->GetXaxis()->SetLabelSize(0.1104);
+    ratio->GetXaxis()->SetTitleSize(0.138);
+    ratio->GetXaxis()->SetTitleFont(42);
+    ratio->GetYaxis()->SetTitle("ratio");
+    ratio->GetYaxis()->CenterTitle(true);
+    ratio->GetYaxis()->SetNdivisions(-502);
+    ratio->GetYaxis()->SetLabelFont(42);
+    ratio->GetYaxis()->SetLabelOffset(0.007);
+    ratio->GetYaxis()->SetLabelSize(0.1104);
+    ratio->GetYaxis()->SetTitleSize(0.138);
+    ratio->GetYaxis()->SetTitleOffset(0.4);
+    ratio->GetYaxis()->SetTitleFont(42);
+    ratio->GetZaxis()->SetLabelFont(42);
+    ratio->GetZaxis()->SetLabelOffset(0.007);
+    ratio->GetZaxis()->SetTitleSize(0.05);
+    ratio->GetZaxis()->SetTitleFont(42);
+
 
     //-----------------------------------------
     // draw histograms
@@ -267,7 +411,7 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
     TLine line;
     ratio->Draw("same");
     line.DrawLine(0,1,350,1);
-    ratio->GetYaxis()->SetTitle("data/pred");
+    ratio->GetYaxis()->SetTitle("ratio");
   }
 
   //-----------------------------------------
