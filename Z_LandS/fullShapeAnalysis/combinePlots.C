d8679834ccc03898096a5c1a8f0372d613a8bf17
benhoob <>
2012-01-19 16:29:12 +0000

    style updates, add smoothHist and by-hand exclusions and add version

--- Z_LandS/fullShapeAnalysis/combinePlots.C
+++ Z_LandS/fullShapeAnalysis/combinePlots.C
@@ -29,23 +29,32 @@ using namespace std;
 
 TGraph* getGraph(bool do3jets,string type){
 
-  float x[4];
-  float y[4];
+  float x[5];
+  float y[5];
   int npoints = -1;
 
   if( !do3jets && type == "nom" ){
-    x[0] = 700;   y[0] = 100;
-    x[1] = 850;   y[1] = 300;
-    x[2] = 925;   y[2] = 500;
-    x[3] = 925;   y[3] = 900;
-    npoints = 4;
+    x[0] =  700;  y[0] = 100;
+    x[1] =  850;  y[1] = 300;
+    x[2] =  925;  y[2] = 500;
+    x[3] =  975;  y[3] = 850;
+    x[4] =  925;  y[4] = 925;
+    npoints = 5;
   }
   else if( !do3jets && type == "down" ){
     x[0] = 500;   y[0] = 100;
     x[1] = 720;   y[1] = 300;
-    x[2] = 800;   y[2] = 500;
-    x[3] = 800;   y[3] = 800;
+    x[2] = 825;   y[2] = 550;
+    x[3] = 825;   y[3] = 800;
     npoints = 4;
+  }
+  else if( !do3jets && type == "up" ){
+    x[0] = 800;   y[0] =  100;
+    x[1] = 950;   y[1] =  200;
+    x[2] = 1025;  y[2] =  400;
+    x[3] = 1100;  y[3] = 1000;
+    x[4] = 1000;  y[4] = 1000;
+    npoints = 5;
   }
   else if( do3jets && type == "nom" ){
     x[0] = 700;   y[0] = 100;
@@ -65,6 +74,47 @@ TGraph* getGraph(bool do3jets,string type){
   TGraph *gr = new TGraph(npoints,x,y);
   return gr;
 
+}
+
+void smoothHist( TH2F* h ){
+
+  vector<int> binx;
+  vector<int> biny;
+  binx.push_back(19);    biny.push_back(8);
+  binx.push_back(33);    biny.push_back(7);
+  binx.push_back(35);    biny.push_back(7);
+
+  binx.push_back(28);    biny.push_back(26);
+  binx.push_back(34);    biny.push_back(30);
+  binx.push_back(34);    biny.push_back(31);
+
+  binx.push_back(42);    biny.push_back(37);
+  binx.push_back(45);    biny.push_back(37);
+  binx.push_back(45);    biny.push_back(36);
+
+  const unsigned int npoints = binx.size();
+
+  for( unsigned int ibin = 1 ; ibin <= 48 ; ibin++ ){
+    for( unsigned int jbin = 1 ; jbin <= 48 ; jbin++ ){
+
+      float val = h->GetBinContent(ibin,jbin);
+
+      if( val < 1e-10 ) continue;
+      //cout << ibin << " " << jbin << " " << val << endl;
+
+      for(unsigned int ipoint = 0 ; ipoint < npoints ; ipoint++ ){
+	if( ibin == binx.at(ipoint) && jbin == biny.at(ipoint) ){
+
+	  float valup  = h->GetBinContent(ibin+1,jbin);
+	  float valdn  = h->GetBinContent(ibin-1,jbin);
+	  float valavg = 0.5 * (valup+valdn);
+
+	  h->SetBinContent(ibin,jbin,valavg);
+	}
+	
+      }
+    }
+  }  
 }
 
 void combinePlots(bool print = false){
@@ -89,22 +139,36 @@ void combinePlots(bool print = false){
   // bool  do3jets        = true;
   // char* title          = "m(#tilde{q}) >> m(#tilde{g}), x = 0.75";
   
-  char* version        = "V00-01-04";
-  char* sample         = "T5zzgmsb";
-  bool  do3jets        = false;
-  char* title          = "m(#tilde{q}) >> m(#tilde{g})";
+  // char* version        = "V00-01-04";
+  // char* sample         = "T5zzgmsb";
+  // bool  do3jets        = false;
+  // char* title          = "m(#tilde{q}) >> m(#tilde{g})";
 
   // char* version        = "V00-01-05";
   // char* sample         = "T5zzgmsb";
   // bool  do3jets        = true;
   // char* title          = "m(#tilde{q}) >> m(#tilde{g})";
 
+  // char* version        = "V00-01-07";
+  // char* sample         = "T5zzgmsb_hadoop";
+  // bool  do3jets        = false;
+  // char* title          = "m(#tilde{q}) >> m(#tilde{g})";
+
+  char* version        = "V00-01-08";
+  char* sample         = "T5zzh";
+  bool  do3jets        = false;
+  char* title          = "m(#tilde{q}) >> m(#tilde{g}), x = 0.25";
+
   char* njets          = "n_{jets} #geq 2";
   if( do3jets )  njets = "n_{jets} #geq 3";
 
+  float ymin = 50.;
+  if( TString(sample).Contains("gmsb") ) ymin = 100.;
+
   TFile *file = TFile::Open(Form("cards/%s/observed_limit.root",version));
   TH2F* hexcl = (TH2F*) file->Get("hexcl");
-  
+  //TH2F* hexcl = (TH2F*) file->Get("hexp");
+  //cout << "USING EXPECTED LIMIT!!!!!!!!!!!!" << endl;
 
   TLatex *t = new TLatex();
   t->SetNDC();
@@ -113,8 +177,13 @@ void combinePlots(bool print = false){
   // calculate efficiency
   //-------------------------------
 
+  char* babyversion = "V00-02-04";
+  if( TString(sample).Contains("hadoop") ) babyversion = "V00-02-05";
+  if( TString(sample).Contains("T5zzh") )  babyversion = "V00-02-05";
+
   TChain *ch = new TChain("T1");
-  ch->Add(Form("output/V00-02-04/%s_baby.root",sample));
+  //ch->Add(Form("output/V00-02-04/%s_baby.root",sample));
+  ch->Add(Form("output/%s/%s_baby.root",babyversion,sample));
 
   TCut zmass("dilmass>81 && dilmass<101");
   TCut njets2("njets >= 2");
@@ -140,7 +209,8 @@ void combinePlots(bool print = false){
   // find excluded points
   //-------------------------------
 
-  TFile *xsecfile = TFile::Open("reference_xSec.root");
+  //TFile *xsecfile = TFile::Open("reference_xSec.root");
+  TFile *xsecfile = TFile::Open("reference_xSec_mg2TeV.root");
   TH1F* refxsec   = (TH1F*) xsecfile->Get("gluino");
 
   TH2F* hexcluded   = new TH2F("hexcluded","hexcluded", 48,0,1200,48,0,1200);
@@ -186,15 +256,19 @@ void combinePlots(bool print = false){
   can->cd(1);
   gPad->SetTopMargin(0.1);
   gPad->SetRightMargin(0.2);
-  //heff->GetXaxis()->SetRangeUser(0,950);
+
+  if( TString(sample).Contains("gmsb") ) smoothHist( heff );
+
+  heff->GetYaxis()->SetRangeUser(ymin,1200);
   heff->GetXaxis()->SetLabelSize(0.035);
   heff->GetYaxis()->SetLabelSize(0.035);
   heff->GetZaxis()->SetLabelSize(0.035);
-  heff->GetYaxis()->SetTitle("#chi^{0}_{1} mass (GeV)");
-  heff->GetXaxis()->SetTitle("gluino mass (GeV)");
-  heff->GetZaxis()->SetTitle("efficiency");
+  heff->GetYaxis()->SetTitle("#chi^{0}_{1} mass [GeV]");
+  heff->GetXaxis()->SetTitle("gluino mass [GeV]");
+  heff->GetZaxis()->SetTitle("A #times #varepsilon (#geq1 Z(ll))");
   heff->Draw("colz");
-  
+  heff->GetYaxis()->SetRangeUser(ymin,1200);
+
   t->SetTextSize(0.04);
 
   if(TString(sample).Contains("gmsb") )  t->DrawLatex(0.2,0.83,"pp #rightarrow #tilde{g}#tilde{g}, #tilde{g} #rightarrow 2j+#chi_{1}^{0}, #chi_{1}^{0} #rightarrow Z G");
@@ -214,18 +288,23 @@ void combinePlots(bool print = false){
   can->cd(2);
   gPad->SetTopMargin(0.1);
   gPad->SetRightMargin(0.2);
+
+  if( TString(sample).Contains("gmsb") ) smoothHist( hexcl );
+
+  hexcl->GetYaxis()->SetRangeUser(ymin,1200);
   //hexcl->GetXaxis()->SetRangeUser(0,950);
   gPad->SetLogz();
   hexcl->GetXaxis()->SetLabelSize(0.035);
   hexcl->GetYaxis()->SetLabelSize(0.035);
   hexcl->GetZaxis()->SetLabelSize(0.035);
-  hexcl->GetYaxis()->SetTitle("#chi^{0}_{1} mass (GeV)");
-  hexcl->GetXaxis()->SetTitle("gluino mass (GeV)");
-  hexcl->GetZaxis()->SetTitle("#sigma upper limit");
+  hexcl->GetYaxis()->SetTitle("#chi^{0}_{1} mass [GeV]");
+  hexcl->GetXaxis()->SetTitle("gluino mass [GeV]");
+  hexcl->GetZaxis()->SetTitle("95% CL upper limit on #sigma [pb]");
   hexcl->Draw("colz");
   hexcl->SetMinimum(0.001);
   hexcl->SetMaximum(10);
-  
+  hexcl->GetYaxis()->SetRangeUser(ymin,1200);
+
   TGraph* gr_excl;      
   TGraph* gr_excl_down;
   TGraph* gr_excl_up;   
@@ -233,7 +312,7 @@ void combinePlots(bool print = false){
   if( TString(sample).Contains("gmsb") ) {
     gr_excl      = getGraph(do3jets,"nom");
     gr_excl_down = getGraph(do3jets,"down");
-    gr_excl_up   = getGraph(do3jets,"nom");
+    gr_excl_up   = getGraph(do3jets,"up");
   }else{
     gr_excl      = getRefXsecGraph(hexcl, "T5zz", 1.0);
     gr_excl_down = getRefXsecGraph(hexcl, "T5zz", 1./3.);
@@ -246,13 +325,13 @@ void combinePlots(bool print = false){
   gr_excl_up->SetLineStyle(2);
   gr_excl_down->SetLineStyle(3);
   gr_excl->Draw("same");
-  //gr_excl_up->Draw("same");
+  gr_excl_up->Draw("same");
   gr_excl_down->Draw("same");
 
 
   TLegend *leg = new TLegend(0.2,0.53,0.45,0.67);
   leg->AddEntry(gr_excl,     "#sigma^{NLO-QCD}","l");
-  //leg->AddEntry(gr_excl_up,  "3 #times #sigma^{NLO-QCD}","l");
+  leg->AddEntry(gr_excl_up,  "3 #times #sigma^{NLO-QCD}","l");
   leg->AddEntry(gr_excl_down,"1/3 #times #sigma^{NLO-QCD}","l");
   leg->SetFillColor(0);
   leg->SetBorderSize(0);
@@ -270,26 +349,38 @@ void combinePlots(bool print = false){
 
   if( print ){
     can->Print(Form("cards/%s/plots/SMS.eps",version));
+    can->Print(Form("cards/%s/plots/SMS.pdf",version));
     can->Print(Form("cards/%s/plots/SMS.png",version));
-    gROOT->ProcessLine(Form(".! ps2pdf cards/%s/plots/SMS.eps cards/%s/plots/SMS.pdf",version,version));
+
+    gROOT->ProcessLine(Form(".! ps2pdf cards/%s/plots/SMS.eps cards/%s/plots/SMS_ppt.pdf",version,version));
   }
 
 
-
+  TFile* fout = TFile::Open(Form("cards/%s/limit.root",version),"RECREATE");
+  fout->cd();
+  hexcl->Write();
+  gr_excl->Write();
+  fout->Close();
 
   TCanvas *c2 = new TCanvas("c2","c2",1500,500);
   c2->Divide(3,1);
 
   c2->cd(1);
+  gPad->SetGridx();
+  gPad->SetGridy();
   hexcluded13->Draw("colz");
   gr_excl_down->Draw();
 
   c2->cd(2);
+  gPad->SetGridx();
+  gPad->SetGridy();
   hexcluded->Draw("colz");
   gr_excl->Draw();
 
   c2->cd(3);
+  gPad->SetGridx();
+  gPad->SetGridy();
   hexcluded3->Draw("colz");
-
+  gr_excl_up->Draw();
 
 }
