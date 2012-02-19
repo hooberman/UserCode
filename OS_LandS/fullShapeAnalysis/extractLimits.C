8fcd7325c047bb73bb0b5966f62602d87a6c7f62
benhoob <>
2012-02-19 00:51:52 +0000

    udpate with additional contours

--- OS_LandS/fullShapeAnalysis/extractLimits.C
+++ OS_LandS/fullShapeAnalysis/extractLimits.C
@@ -28,7 +28,7 @@
 
 using namespace std;
 
-char* version             = "V00-00-04";
+char* version             = "V00-00-08";
 //bool  writeExpectedLimits = false;
 
 bool fileInList(string thisfilename);
@@ -45,10 +45,16 @@ void extractLimits( bool print = false ){
   const int   nm12points   = 38;
   const float m12min       = 20.;
   const float m12max       = 780.;
+
   TH2F* hexcl    = new TH2F( "hexcl"    , "hexcl"    , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
   TH2F* hexp     = new TH2F( "hexp"     , "hexp"     , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
   TH2F* hexpp1   = new TH2F( "hexpp1"   , "hexpp1"   , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
   TH2F* hexpm1   = new TH2F( "hexpm1"   , "hexpm1"   , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
+
+  TH2F* hvalexcl    = new TH2F( "hvalexcl"    , "hvalexcl"    , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
+  TH2F* hvalexp     = new TH2F( "hvalexp"     , "hvalexp"     , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
+  TH2F* hvalexpp1   = new TH2F( "hvalexpp1"   , "hvalexpp1"   , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
+  TH2F* hvalexpm1   = new TH2F( "hvalexpm1"   , "hvalexpm1"   , nm0points,m0min-10,m0max-10,nm12points,m12min-10,m12max-10);
 
   //------------------------------------------
   // loop over CMSSM points
@@ -71,7 +77,11 @@ void extractLimits( bool print = false ){
 
       hexcl->SetBinContent(m0bin,m12bin,0);
 
-      if( m0bin>49 ) continue;
+      if( m0bin == 56 && m12bin == 5 ) continue;
+      if( m0bin == 71 && m12bin == 6 ) continue;
+      if( m0bin == 78 && m12bin == 6 ) continue;
+
+      //if( m0bin>5 ) continue;
       //if( m0==80 && m12==400 ) cout << "FOUND LM6 " << endl;
       //else continue;
 
@@ -97,18 +107,27 @@ void extractLimits( bool print = false ){
       }
       
       else{
-	cout << "Writing limits to histos" << endl;
+	cout << endl;
+	cout << "---------------------------------------------------------------" << endl;
+	cout << "Writing limits to histos: m0 " << m0 << " m12 " << m12 << endl;
 	cout << "Observed      " << mylimit.obs << endl;
 	cout << "Expected      " << mylimit.exp << endl;
 	cout << "Expected(+1)  " << mylimit.expp1 << endl;
 	cout << "Expected(-1)  " << mylimit.expm1 << endl;
+	cout << "---------------------------------------------------------------" << endl;
+	cout << endl;
 
 	hexcl-> SetBinContent(m0bin,m12bin, mylimit.obs   < 1.0 ? 1 : -1 );
 	hexp->  SetBinContent(m0bin,m12bin, mylimit.exp   < 1.0 ? 1 : -1 );
 	hexpp1->SetBinContent(m0bin,m12bin, mylimit.expp1 < 1.0 ? 1 : -1 );
 	hexpm1->SetBinContent(m0bin,m12bin, mylimit.expm1 < 1.0 ? 1 : -1 );
-      }
 
+	hvalexcl-> SetBinContent(m0bin,m12bin, mylimit.obs   );
+	hvalexp->  SetBinContent(m0bin,m12bin, mylimit.exp   );
+	hvalexpp1->SetBinContent(m0bin,m12bin, mylimit.expp1 );
+	hvalexpm1->SetBinContent(m0bin,m12bin, mylimit.expm1 );
+      }
+      
 
       // TTree* t = (TTree*) f->Get("T");
       // Double_t limit;
@@ -129,6 +148,8 @@ void extractLimits( bool print = false ){
       // 	  *filelist << Form("cards/%s/CMSSM_%i_%i.txt_BayesianBayesian_limitbands.root",version,m0bin,m12bin)                  << endl;
       // 	}
       // }
+
+      f->Close();
     }
   }
 
@@ -138,29 +159,33 @@ void extractLimits( bool print = false ){
   // draw exclusion histogram
   //------------------------------------------
 
-  TCanvas *can = new TCanvas("can","can",1000,800);
-  can->cd();
-  gPad->SetRightMargin(0.2);
-  hexcl->GetXaxis()->SetRangeUser(0,2000);
-  hexcl->GetYaxis()->SetRangeUser(100,700);
-  hexcl->GetXaxis()->SetLabelSize(0.03);
-  hexcl->GetYaxis()->SetLabelSize(0.03);
-  hexcl->GetXaxis()->SetTitle("m_{0} (GeV)");
-  hexcl->GetYaxis()->SetTitle("m_{1/2} (GeV)");
-  hexcl->Draw("colz");
+  // TCanvas *can = new TCanvas("can","can",1000,800);
+  // can->cd();
+  // gPad->SetRightMargin(0.2);
+  // hexcl->GetXaxis()->SetRangeUser(0,2000);
+  // hexcl->GetYaxis()->SetRangeUser(100,700);
+  // hexcl->GetXaxis()->SetLabelSize(0.03);
+  // hexcl->GetYaxis()->SetLabelSize(0.03);
+  // hexcl->GetXaxis()->SetTitle("m_{0} (GeV)");
+  // hexcl->GetYaxis()->SetTitle("m_{1/2} (GeV)");
+  // hexcl->Draw("colz");
 
   TFile* outfile = TFile::Open(Form("cards/%s/observed_limit.root",version),"RECREATE");
   hexcl->Write();
   hexp->Write();
   hexpp1->Write();
   hexpm1->Write();
+  hvalexcl->Write();
+  hvalexp->Write();
+  hvalexpp1->Write();
+  hvalexpm1->Write();
   outfile->Close();
 
-  if( print ){
-    can->Print(Form("cards/%s/CMSSM.eps",version));
-    can->Print(Form("cards/%s/CMSSM.png",version));
-    gROOT->ProcessLine(Form(".! ps2pdf cards/%s/CMSSM.eps cards/%s/CMSSM.pdf",version,version));
-  }
+  // if( print ){
+  //   can->Print(Form("cards/%s/CMSSM.eps",version));
+  //   can->Print(Form("cards/%s/CMSSM.png",version));
+  //   gROOT->ProcessLine(Form(".! ps2pdf cards/%s/CMSSM.eps cards/%s/CMSSM.pdf",version,version));
+  // }
 
 }
 
