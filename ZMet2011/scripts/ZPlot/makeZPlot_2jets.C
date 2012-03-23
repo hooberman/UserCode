07562249fbac8995bcb0d2e030ceeeea26d00539
benhoob <>
2012-03-23 12:07:48 +0000

    change color of OF

--- ZMet2011/scripts/ZPlot/makeZPlot_2jets.C
+++ ZMet2011/scripts/ZPlot/makeZPlot_2jets.C
@@ -210,7 +210,8 @@ void doPlot( TCanvas *can , TH1F* hist_VV , TH1F* hist_OF , TH1F* hist_photon , 
   hist_data->Draw("E1");
 
   hist_VV->SetFillColor(kGreen-10);
-  hist_OF->SetFillColor(kMagenta+2);
+  //hist_OF->SetFillColor(kMagenta+2);
+  hist_OF->SetFillColor(kMagenta-8);
   //hist_VV->SetFillStyle(3006);
   hist_photon->SetLineColor(kRed);
   hist_QCD->SetLineColor(kBlue);
