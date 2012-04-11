2b862049dd2556d6a47bab4000b14d87ab5616b4
benhoob <>
2012-04-11 11:29:55 +0000

    update vtx reweighting function

--- ZMet2012/looper/Z_looper.C
+++ ZMet2012/looper/Z_looper.C
@@ -254,7 +254,7 @@ void Z_looper::ScanChain (TChain* chain, const char* prefix, bool isData,
     set_vtxreweight_rootfile("vtxreweight_GMSB_4p7fb_Zselection.root",true);
   }
   else{
-    set_vtxreweight_rootfile("vtxreweight_Summer11MC_PUS4_4p7fb_Zselection.root",true);
+    set_vtxreweight_rootfile("vtxreweight_zjets_32pb_2012_Zselection.root",true);
   }
 
 
