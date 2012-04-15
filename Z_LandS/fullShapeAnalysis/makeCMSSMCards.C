84ed872ac15ed91c3a4d553772883c4dddaa68e8
benhoob <>
2012-04-15 15:16:58 +0000

    update for T5zzh

--- Z_LandS/fullShapeAnalysis/makeCMSSMCards.C
+++ Z_LandS/fullShapeAnalysis/makeCMSSMCards.C
@@ -68,8 +68,8 @@ void makeCMSSMCards(){
   //---------------------------------------
   
   TChain *ch = new TChain("T1");
-  ch->Add("output/V00-02-04/T5zzgmsb_baby.root");
-  char* version = "V00-03-06";
+  ch->Add("output/V00-02-05/T5zzh_baby.root");
+  char* version = "V00-03-07";
 
   bool do3jets = false;
 
