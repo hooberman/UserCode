45b464271418ed044660536a681221af67c4301f
benhoob <>
2012-05-16 14:12:17 +0000

    add DoubleElectron

--- ZMet2012/photonLooper/doAll_makePhotonBabies.C
+++ ZMet2012/photonLooper/doAll_makePhotonBabies.C
@@ -3,5 +3,6 @@
   gROOT->ProcessLine(".L runMakePhotonBabies.C+");
   
   //--photon+jets---
-  runMakePhotonBabies("Photon"                , true  );   //Photon data
+  //runMakePhotonBabies("Photon"                , true  );   // Photon data
+  runMakePhotonBabies("DoubleElectron"        , true  );   // DoubleElectron data
 }
