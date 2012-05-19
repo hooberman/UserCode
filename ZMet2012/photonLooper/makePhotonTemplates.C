7b44eb8257faeecdcb041fb12939931f5cace9f2
benhoob <>
2012-05-19 19:43:44 +0000

    lots of updates

--- ZMet2012/photonLooper/makePhotonTemplates.C
+++ ZMet2012/photonLooper/makePhotonTemplates.C
@@ -5,7 +5,10 @@
 #include <math.h>
 #include <fstream>
 
+#ifndef __CINT__
 #include "../CORE/metTemplatesSelections.cc"
+#endif
+
 #include "TChain.h"
 #include "TRandom3.h"
 #include "TDirectory.h"
@@ -22,7 +25,7 @@
 #include "Math/VectorUtil.h"
 #include "TLorentzVector.h"
 
-bool debug = true;
+bool debug          = true;
 
 using namespace std;
 
@@ -30,9 +33,16 @@ inline double fround(double n, double d){
   return floor(n * pow(10., d) + .5) / pow(10., d);
 }
 
-void makePhotonTemplates::ScanChain ( TChain* chain , char* iter ){
+void makePhotonTemplates::ScanChain ( TChain* chain , char* iter , char* sample ){
 
+  bool useHGGTriggers = false;
   
+  if( TString(sample).Contains("DoubleElectron") ) useHGGTriggers = true;
+
+  cout << "Sample : " << sample << endl;
+  if( useHGGTriggers ) cout << "Using H->gg triggers" << endl;
+  else                 cout << "Using standard triggers" << endl;
+
   int npass = 0;
   bookHistos();
   
@@ -79,17 +89,38 @@ void makePhotonTemplates::ScanChain ( TChain* chain , char* iter ){
         }
       }
 
-      //kinematic selection
-      if( nJets_ < 2 )        continue;
+      int h20 = hlt20_;
+      int h30 = hlt30_;
+      int h50 = hlt50_;
+      int h75 = hlt75_;
+      int h90 = hlt90_;
+      
+      if( useHGGTriggers ){
+	h20 = hgg22_;
+	h30 = hgg36_;
+	h50 = hgg50_;
+	h75 = hgg75_;
+	h90 = hgg90_;
+      }
 
-      //cleaning
-      if( jet_pt_ - etg_ < -5 )                                      continue; // cleaning
-      if( fabs( etag_ ) > 2 )                                        continue; // photon eta < 2
-      if( pfjetid_ != 1 )                                            continue; // pass PFJetID
-      if( hlt20_ < 1 && hlt30_ < 1 && hlt50_ < 1 && hlt75_ < 1 )     continue; // require trig
+      // event selection 
+      if( nJets_ < 2 )                                                        continue; // >=2 jets
+      if( etg_ <  20 )                                                        continue; // photon pt  > 20 GeV
+      if( fabs( etag_ ) > 2 )                                                 continue; // photon eta < 2
+      if( hoe_ > 0.1 )                                                        continue; // H/E < 0.1
+      if( photon_pixelseed_ == 1 )                                            continue; // veto pixel match
+      if( jetneutralemfrac_ < 0.7 )                                           continue; // jet neutral EM fraction cut
+      if( jet_pt_     - etg_ < -5 )                                           continue; // pfjet cleaning
+      if( calojet_pt_ - etg_ < -5 )                                           continue; // calojet cleaning
+      if( elveto_ == 1 )                                                      continue; // remove photons with nearby electrons
+      if( maxleppt_ > 20.0 )                                                  continue; // veto leptons pt > 20 GeV
+      if( acos( cos( phig_ - pfmetphi_ ) ) < 0.14 )                           continue; // kill photons aligned with MET
+
+      // //if( pfjetid_ != 1 )                                                     continue; // pass PFJetID
+      if( h20 < 1 && h30 < 1 && h50 < 1 && h75 < 1 && h90 < 1 )                    continue; // require trig
 
       int iJetBin          = getJetBin       ( nJets_    );
-      int iSumJetPtBin     = getSumJetPtBin  ( sumJetPt_ );
+      int iSumJetPtBin     = getSumJetPtBin  ( ht_       );
       int iBosonPtBin      = getBosonPtBin   ( etg_      );
       int iVtxBin          = getVtxBin       ( nvtx_     );
       float templateWeight = 1;
@@ -105,62 +136,67 @@ void makePhotonTemplates::ScanChain ( TChain* chain , char* iter ){
       //fill templates binned by njets, sumjetpt
       fillUnderOverFlow( tcmetTemplate_combined[ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
       fillUnderOverFlow( pfmetTemplate_combined[ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
-      
+
       ++npass;
 
       int iTrigBin = -1;
 
+      if( h20 > 0 )  fillUnderOverFlow( hphotonPt20 , etg_ , h20 );
+      if( h30 > 0 )  fillUnderOverFlow( hphotonPt30 , etg_ , h30 );
+      if( h50 > 0 )  fillUnderOverFlow( hphotonPt50 , etg_ , h50 );
+      if( h75 > 0 )  fillUnderOverFlow( hphotonPt70 , etg_ , h75 );
+      if( h90 > 0 )  fillUnderOverFlow( hphotonPt90 , etg_ , h90 );
 
-      if( hlt75_ > 0 ){
-        
-        templateWeight = hlt75_;
+      if( h90 > 0 ){
+        templateWeight = h90;
+        iTrigBin = 4;
+
+        fillUnderOverFlow( tcmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
+        fillUnderOverFlow( pfmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
+      }
+      
+      else if( h75 > 0 ){
+        templateWeight = h75;
         iTrigBin = 3;
 
         fillUnderOverFlow( tcmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
         fillUnderOverFlow( pfmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
-
-        fillUnderOverFlow( hphotonPt70 , etg_ , templateWeight );
       }
 
-      else if( hlt50_ > 0 ){
-	
-        templateWeight = hlt50_;
+      else if( h50 > 0 ){
+        templateWeight = h50;
         iTrigBin = 2;
 
         fillUnderOverFlow( tcmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
         fillUnderOverFlow( pfmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
-
-        fillUnderOverFlow( hphotonPt50 , etg_ , templateWeight );
       }
 
-      else if( hlt30_ > 0 ){
-        
-        templateWeight = hlt30_;
+      else if( h30 > 0 ){
+        templateWeight = h30;
         iTrigBin = 1;
 
         fillUnderOverFlow( tcmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
         fillUnderOverFlow( pfmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
-
-        fillUnderOverFlow( hphotonPt30 , etg_ , templateWeight );
       }
 
-      else if( hlt20_ > 0 ){
-        
-        templateWeight = hlt20_;
+      else if( h20 > 0 ){
+        templateWeight = h20;
         iTrigBin = 0;
 
         fillUnderOverFlow( tcmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  tcmet_    , templateWeight );
         fillUnderOverFlow( pfmetTemplate_photon[ iTrigBin ][ iJetBin ][ iSumJetPtBin ]    ,  pfmet_    , templateWeight );
-
-        fillUnderOverFlow( hphotonPt20 , etg_ , templateWeight );
       }
 
-
-
+      else{
+	cout << "NO TRIGGERS PASS!!!" << endl;
+	exit(0);
+      }
 
     } // end loop over events
   } // end loop over files
       
+
+
   cout << npass << " events passing selection" << endl;
   if (nEventsChain != nEventsTotal)
     std::cout << "ERROR: number of events from files is not equal to total number of events" << std::endl;
@@ -210,7 +246,7 @@ void makePhotonTemplates::ScanChain ( TChain* chain , char* iter ){
       }
     }
 
-    for( int iTrigBin = 0 ; iTrigBin < 4 ; ++iTrigBin ){
+    for( int iTrigBin = 0 ; iTrigBin < 5 ; ++iTrigBin ){
       for( int iJetBin = 0 ; iJetBin < nJetBins ; iJetBin++ ){
         for( int iSumJetPtBin = 0 ; iSumJetPtBin < nSumJetPtBins ; iSumJetPtBin++ ){
           
@@ -228,9 +264,18 @@ void makePhotonTemplates::ScanChain ( TChain* chain , char* iter ){
     // make histos rootfile
     TDirectory *rootdir = gDirectory->GetDirectory("Rint:");
     rootdir->cd();
-    cout << "Writing templates to " << Form("../templates/%s/photon_templates.root",iter) << endl;
-    saveHist(Form("../templates/%s/photon_templates.root",iter));
-    deleteHistos();
+    cout << "Writing templates to " << Form("../photon_output/%s/%s_templates.root",iter,sample) << endl;
+    saveHist(Form("../photon_output/%s/%s_templates.root",iter,sample));
+    //deleteHistos();
+
+    TFile* fout = TFile::Open(Form("../photon_output/%s/%s_templateHistos.root",iter,sample),"RECREATE");
+    fout->cd();
+    hphotonPt20->Write();
+    hphotonPt30->Write();
+    hphotonPt50->Write();
+    hphotonPt70->Write();
+    hphotonPt90->Write();
+    fout->Close();
   
 } // end ScanChain
 
@@ -256,8 +301,9 @@ void makePhotonTemplates::bookHistos(){
   hphotonPt30  = new TH1F("hphotonPt30", "",500,0,500);
   hphotonPt50  = new TH1F("hphotonPt50", "",500,0,500);
   hphotonPt70  = new TH1F("hphotonPt70", "",500,0,500);
+  hphotonPt90  = new TH1F("hphotonPt90", "",500,0,500);
 
-  int maxmet = 250;
+  int maxmet = 100;
 
   for( int iJetBin = 0 ; iJetBin < nJetBins ; iJetBin++ ){
     for( int iSumJetPtBin = 0 ; iSumJetPtBin < nSumJetPtBins ; iSumJetPtBin++ ){
@@ -323,9 +369,9 @@ void makePhotonTemplates::bookHistos(){
     }
   }
   
-  char* trigName[4]={"HLT20","HLT30","HLT50","HLT75"};
+  char* trigName[5]={(char*)"HLT20",(char*)"HLT30",(char*)"HLT50",(char*)"HLT75",(char*)"HLT90"};
 
-  for( int iTrigBin = 0 ; iTrigBin < 4 ; iTrigBin++ ){
+  for( int iTrigBin = 0 ; iTrigBin < 5 ; iTrigBin++ ){
     for( int iJetBin = 0 ; iJetBin < nJetBins ; iJetBin++ ){
       for( int iSumJetPtBin = 0 ; iSumJetPtBin < nSumJetPtBins ; iSumJetPtBin++ ){
         
@@ -353,19 +399,32 @@ void makePhotonTemplates::bookHistos(){
 
 void makePhotonTemplates::setBranches (TTree* tree){
   
-  tree->SetBranchAddress("tcmet"	,       &tcmet_         );
-  tree->SetBranchAddress("pfmet"	,       &pfmet_         );
-  tree->SetBranchAddress("njets"	,       &nJets_         );
-  tree->SetBranchAddress("sumjetpt"	,       &sumJetPt_      );
-  tree->SetBranchAddress("nvtx"		,       &nvtx_          );
-  tree->SetBranchAddress("jetpt"	,       &jet_pt_        );  
-  tree->SetBranchAddress("etag"		,       &etag_          ); 
-  tree->SetBranchAddress("etg"		,       &etg_           ); 
-  tree->SetBranchAddress("pfjetid"	,       &pfjetid_       );     
-  tree->SetBranchAddress("hlt20"	,	&hlt20_		);
-  tree->SetBranchAddress("hlt30"	,	&hlt30_		);
-  tree->SetBranchAddress("hlt50"	,	&hlt50_		);
-  tree->SetBranchAddress("hlt75"	,	&hlt75_		);
-  tree->SetBranchAddress("hlt125"	,	&hlt125_	);
+  tree->SetBranchAddress("tcmet"	       ,        &tcmet_                 );
+  tree->SetBranchAddress("pfmet"	       ,        &pfmet_                 );
+  tree->SetBranchAddress("pfmetphi"	       ,        &pfmetphi_              );
+  tree->SetBranchAddress("njets"	       ,        &nJets_                 );
+  tree->SetBranchAddress("ht"	               ,        &ht_                    );
+  tree->SetBranchAddress("nvtx"		       ,        &nvtx_                  );
+  tree->SetBranchAddress("jetpt"	       ,        &jet_pt_                );
+  tree->SetBranchAddress("calojetpt"	       ,	&calojet_pt_		);  
+  tree->SetBranchAddress("etag"		       ,        &etag_                  ); 
+  tree->SetBranchAddress("phig"		       ,        &phig_                  ); 
+  tree->SetBranchAddress("etg"		       ,        &etg_                   ); 
+  tree->SetBranchAddress("pfjetid"	       ,        &pfjetid_               );     
+  tree->SetBranchAddress("hlt20" 	       ,	&hlt20_		        );
+  tree->SetBranchAddress("hlt30"	       ,	&hlt30_		        );
+  tree->SetBranchAddress("hlt50"	       ,	&hlt50_		        );
+  tree->SetBranchAddress("hlt75"	       ,	&hlt75_		        );
+  tree->SetBranchAddress("hlt90"   	       ,	&hlt90_		        );
+  tree->SetBranchAddress("hgg22"	       ,	&hgg22_		        );
+  tree->SetBranchAddress("hgg36"	       ,	&hgg36_	        	);
+  tree->SetBranchAddress("hgg50"       	       ,	&hgg50_		        );
+  tree->SetBranchAddress("hgg75"	       ,	&hgg75_		        );
+  tree->SetBranchAddress("hgg90"	       ,	&hgg90_		        );
+  tree->SetBranchAddress("hoe"	               ,	&hoe_	        	);
+  tree->SetBranchAddress("photon_pixelseed"    ,	&photon_pixelseed_	);
+  tree->SetBranchAddress("maxleppt"	       ,	&maxleppt_		);
+  tree->SetBranchAddress("elveto"	       ,	&elveto_		);
+  tree->SetBranchAddress("jetneutralemfrac"    ,        &jetneutralemfrac_      );
 
 }
--- ZMet2012/photonLooper/makePhotonTemplates.h
+++ ZMet2012/photonLooper/makePhotonTemplates.h
@@ -18,7 +18,7 @@ class makePhotonTemplates
   };
   
   void setBranches (TTree *tree);
-  void ScanChain (TChain*,char*);
+  void ScanChain (TChain*,char*,char*);
   void bookHistos();
   void fillUnderOverFlow(TH1F *h1, float value, float weight);
   
@@ -26,26 +26,42 @@ class makePhotonTemplates
 
   Float_t   tcmet_;
   Float_t   pfmet_;
+  Float_t   pfmetphi_;
   Int_t     nJets_;
-  Float_t   sumJetPt_;
+  Float_t   ht_;
   Int_t     nvtx_;
   Float_t   maxjetpt_;
   Float_t   prescale_;
   Float_t   jet_pt_;
+  Float_t   calojet_pt_;
   Float_t   etg_;
   Float_t   etag_;
+  Float_t   phig_;
   Int_t     pfjetid_;
+  Float_t   hoe_;
+  Int_t     photon_pixelseed_;
+  Float_t   maxleppt_;
+  Int_t     elveto_;
+  Float_t   jetneutralemfrac_;
 
   Int_t     hlt20_;
   Int_t     hlt30_;
   Int_t     hlt50_;
   Int_t     hlt75_;
+  Int_t     hlt90_;
   Int_t     hlt125_;
+
+  Int_t     hgg22_;
+  Int_t     hgg36_;
+  Int_t     hgg50_;
+  Int_t     hgg75_;
+  Int_t     hgg90_;
 
   TH1F* hphotonPt20;
   TH1F* hphotonPt30;
   TH1F* hphotonPt50;
   TH1F* hphotonPt70;
+  TH1F* hphotonPt90;
 
   TH1F* tcmetTemplate[3][7][4];
   TH1F* pfmetTemplate[3][7][4];
@@ -56,8 +72,8 @@ class makePhotonTemplates
   TH1F* tcmetTemplate_combined[3][7];
   TH1F* pfmetTemplate_combined[3][7];
 
-  TH1F* tcmetTemplate_photon[4][3][7];
-  TH1F* pfmetTemplate_photon[4][3][7];
+  TH1F* tcmetTemplate_photon[5][3][7];
+  TH1F* pfmetTemplate_photon[5][3][7];
   
 };
 
