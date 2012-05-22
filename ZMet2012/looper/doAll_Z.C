9d89e13d346edff1c577a53206c293faf799e296
benhoob <>
2012-05-22 19:23:40 +0000

    update to V00-00-06, run on 2012B merged samples

--- ZMet2012/looper/doAll_Z.C
+++ ZMet2012/looper/doAll_Z.C
@@ -1,12 +1,12 @@
 {
   gROOT->ProcessLine(".L histtools.C+");
-  gSystem->Load("../Tools/MiniFWLite/libMiniFWLite.so");
+  //gSystem->Load("../Tools/MiniFWLite/libMiniFWLite.so");
 
   gROOT->ProcessLine(".L runZLooper.C+");
 
   runZLooper("data"                  , true  );
-  runZLooper("zjets"                 , false );  
-  runZLooper("ttbar"                 , false );  
+  //runZLooper("zjets"                 , false );  
+  //runZLooper("ttbar"                 , false );  
 
 }
 
