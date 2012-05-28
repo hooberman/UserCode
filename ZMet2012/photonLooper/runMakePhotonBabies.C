e47bdbac6232c543c3e58a1f667f7d5eb28d4227
benhoob <>
2012-05-28 22:43:52 +0000

    2012B unmerged samples, V00-00-06

--- ZMet2012/photonLooper/runMakePhotonBabies.C
+++ ZMet2012/photonLooper/runMakePhotonBabies.C
@@ -36,7 +36,7 @@ void runMakePhotonBabies(char* prefix , bool isData = true, float kFactor = 1.){
   if( strcmp( prefix , "Photon" ) == 0 ){
     //pickSkimIfExists(ch,"/hadoop/cms/store/user/cwelke/CMSSW_5_2_3_patch4_V05-02-27/Photon_Run2012A-PromptReco-v1_AOD/unmerged/store*root");
 
-    pickSkimIfExists(ch,"/hadoop/cms/store/user/cwelke/CMSSW_5_2_3_patch4_V05-02-27/SinglePhoton_Run2012B-PromptReco-v1_AOD/merged/merged*root");
+    pickSkimIfExists(ch,"/hadoop/cms/store/user/cwelke/CMSSW_5_2_3_patch4_V05-02-27/SinglePhoton_Run2012B-PromptReco-v1_AOD/unmerged/store*root");
   }
 
   //-----------------------------------------------------------------------------------
@@ -44,7 +44,7 @@ void runMakePhotonBabies(char* prefix , bool isData = true, float kFactor = 1.){
   else if( strcmp( prefix , "DoubleElectron" ) == 0 ){
     //pickSkimIfExists(ch,"/hadoop/cms/store/user/yanjuntu/CMSSW_5_2_3_patch4_V05-02-27/DoubleElectron_Run2012A-PromptReco-v1_AOD/unmerged/store*root");
 
-    pickSkimIfExists(ch,"/hadoop/cms/store/user/yanjuntu/CMSSW_5_2_3_patch4_V05-02-27/DoubleElectron_Run2012B-PromptReco-v1_AOD/merged/merged*root");
+    pickSkimIfExists(ch,"/hadoop/cms/store/user/yanjuntu/CMSSW_5_2_3_patch4_V05-02-27/DoubleElectron_Run2012B-PromptReco-v1_AOD/unmerged/store*root");
   }
 
   //-----------------------------------------------------------------------------------
