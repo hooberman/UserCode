7765d0a17e5fa889fdc36c0fa16e408d4360c253
benhoob <>
2012-05-29 18:30:11 +0000

    update version

--- ZMet2012/looper/doAll_baby.C
+++ ZMet2012/looper/doAll_baby.C
@@ -3,11 +3,10 @@
 #include "babylooper.h"
   
   gROOT->ProcessLine(".L histtools.C+");
-  //gSystem->Load("Tools/MiniFWLite/libMiniFWLite.so");
   gROOT->ProcessLine(".L runBabyLooper.C+");
   
-  char* Z_version        = "V00-00-05";
-  char* template_version = "V00-00-03";
+  char* Z_version        = "V00-00-10";
+  char* template_version = "V00-00-07";
   
   runBabyLooper(Z_version,template_version, "data"      , true   , babylooper::e_ZSelection  );
   //runBabyLooper(Z_version,template_version, "ttbar"     , false  , babylooper::e_ZSelection  );
