cfb9c94de309c0270eb4492c6debe74ea9dbe389
benhoob <>
2012-06-22 15:58:39 +0000

    run on 10% of z+jets and ttbar

--- ZMet2012/looper/doAll_Z.C
+++ ZMet2012/looper/doAll_Z.C
@@ -4,13 +4,13 @@
 
   gROOT->ProcessLine(".L runZLooper.C+");
 
-  runZLooper("dataskim"                , true  );
+  //runZLooper("dataskim"                , true  );
   runZLooper("zjets"                   , false );  
-  //runZLooper("ttbar"                   , false );  
-  runZLooper("wz"                      , false );  
-  runZLooper("zz"                      , false );  
-  runZLooper("ww"                      , false );  
-  runZLooper("t"                       , false );  
+  runZLooper("ttbar"                   , false );  
+  //runZLooper("wz"                      , false );  
+  //runZLooper("zz"                      , false );  
+  //runZLooper("ww"                      , false );  
+  //runZLooper("t"                       , false );  
 
   //runZLooper("testfilter_newJEC"     , true  );  
   //runZLooper("RelValZEE"             , false );  
