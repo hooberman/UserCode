6248d070e38f7edf6f1ede428afe1d8b34ffd07f
benhoob <>
2012-06-26 17:07:58 +0000

    update samples

--- ZMet2012/looper/doAll_baby.C
+++ ZMet2012/looper/doAll_baby.C
@@ -5,12 +5,12 @@
   gROOT->ProcessLine(".L histtools.C+");
   gROOT->ProcessLine(".L runBabyLooper.C+");
   
-  char* Z_version        = "V00-00-16";
-  char* template_version = "V00-00-11";
+  char* Z_version        = "V00-00-17";
+  char* template_version = "V00-00-12";
   
-  runBabyLooper(Z_version,template_version, "dataskim"  , true   , babylooper::e_ZSelection  );
+  //runBabyLooper(Z_version,template_version, "dataskim"  , true   , babylooper::e_ZSelection  );
   //runBabyLooper(Z_version,template_version, "wz"      , false  , babylooper::e_ZSelection  );
-  //runBabyLooper(Z_version,template_version, "zz"        , false  , babylooper::e_ZSelection  );
+  runBabyLooper(Z_version,template_version, "zz"        , false  , babylooper::e_ZSelection  );
   //runBabyLooper(Z_version,template_version, "wz"        , false  , babylooper::e_ZSelection  );
   //runBabyLooper(Z_version,template_version, "zjets"     , false  , babylooper::e_ZSelection  );
   //runBabyLooper(Z_version,template_version, "LM4"       , false  , babylooper::e_ZSelection  );
