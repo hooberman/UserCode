e9a74b8f0e0c406c6b69f4b135b41fb172c28fe2
vimartin <>
2012-06-30 21:42:39 +0000

    including alternative samples

--- SingleLepton2011/looper/processBaby.C
+++ SingleLepton2011/looper/processBaby.C
@@ -84,25 +84,35 @@ void processBaby( TString outfileid = "merged_ntuple_35", TString infile = "/nfs
   //-------------------------------------
   char* sample;
   //MC
-  if (infile.Contains("TTJets"))                            sample = Form("ttall_%s",        outfileid.Data());
-  else if (infile.Contains("WJetsToLNu"))                   sample = Form("wjets_%s",        outfileid.Data());
-  else if (infile.Contains("DYJetsToLL"))                   sample = Form("dy_%s",           outfileid.Data());
-  else if (infile.Contains("T_TuneZ2_s-channel"))           sample = Form("tschan_%s",       outfileid.Data());
-  else if (infile.Contains("Tbar_TuneZ2_s-channel"))        sample = Form("tbarschan_%s",    outfileid.Data());
-  else if (infile.Contains("T_TuneZ2_t-channel"))           sample = Form("ttchan_%s",       outfileid.Data());
-  else if (infile.Contains("Tbar_TuneZ2_t-channel"))        sample = Form("tbartchan_%s",    outfileid.Data());
-  else if (infile.Contains("T_TuneZ2_tW-channel"))          sample = Form("ttWchan_%s",      outfileid.Data());
-  else if (infile.Contains("Tbar_TuneZ2_tW-channel"))       sample = Form("tbartWchan_%s",   outfileid.Data());
-  else if (infile.Contains("WW_TuneZ2"))                    sample = Form("ww_%s",           outfileid.Data());
-  else if (infile.Contains("WZ_TuneZ2"))                    sample = Form("wz_%s",           outfileid.Data());
-  else if (infile.Contains("ZZ_TuneZ2"))                    sample = Form("zz_%s",           outfileid.Data());
-  else if (infile.Contains("QCD_Pt-15to20_MuPt5Enriched"))  sample = Form("MuQCD15to20_%s",  outfileid.Data());
-  else if (infile.Contains("QCD_Pt-20to30_MuPt5Enriched"))  sample = Form("MuQCD20to30_%s",  outfileid.Data());
-  else if (infile.Contains("QCD_Pt-30to50_MuPt5Enriched"))  sample = Form("MuQCD30to50_%s",  outfileid.Data());
-  else if (infile.Contains("QCD_Pt-50to80_MuPt5Enriched"))  sample = Form("MuQCD50to80_%s",  outfileid.Data());
-  else if (infile.Contains("QCD_Pt-80to120_MuPt5Enriched")) sample = Form("MuQCD80to120_%s", outfileid.Data());
-  else if (infile.Contains("SMS-T2tt"))                     sample = Form("T2tt_%s",         outfileid.Data());
-  else if (infile.Contains("WWJetsTo2L2Nu"))                sample = Form("ww2l2nujets_%s",  outfileid.Data());
+  if (infile.Contains("TTJets_TuneZ2_7TeV"))                   sample = Form("ttall_%s",        outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_scaleup_7TeV"))      sample = Form("tt_scaleup_%s",   outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_scaledown_7TeV"))    sample = Form("tt_scaledw_%s",   outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_matchingup_7TeV"))   sample = Form("tt_matchup_%s",   outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_matchingdown_7TeV")) sample = Form("tt_matchdw_%s",   outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_mass178_5_7TeV"))    sample = Form("tt_massup_%s",    outfileid.Data());
+  else if (infile.Contains("TTjets_TuneZ2_mass166_5_7TeV"))    sample = Form("tt_massdw_%s",    outfileid.Data());
+  else if (infile.Contains("TT_TuneZ2_7TeV-pythia6-tauola"))   sample = Form("tt_pythia_%s",    outfileid.Data());
+  else if (infile.Contains("TT_TuneZ2_7TeV-mcatnlo"))          sample = Form("tt_mcatnlo_%s",   outfileid.Data());
+  else if (infile.Contains("TT_TuneZ2_7TeV-powheg-tauola"))    sample = Form("tt_powheg_%s",    outfileid.Data());
+  else if (infile.Contains("TTTo2L2Nu2B_7TeV-powheg-pythia6")) sample = Form("tt_notauola_%s",  outfileid.Data());
+  else if (infile.Contains("WJetsToLNu"))                      sample = Form("wjets_%s",        outfileid.Data());
+  else if (infile.Contains("DYJetsToLL"))                      sample = Form("DYtot_%s",        outfileid.Data());
+  else if (infile.Contains("T_TuneZ2_s-channel"))              sample = Form("tschan_%s",       outfileid.Data());
+  else if (infile.Contains("Tbar_TuneZ2_s-channel"))           sample = Form("tbarschan_%s",    outfileid.Data());
+  else if (infile.Contains("T_TuneZ2_t-channel"))              sample = Form("ttchan_%s",       outfileid.Data());
+  else if (infile.Contains("Tbar_TuneZ2_t-channel"))           sample = Form("tbartchan_%s",    outfileid.Data());
+  else if (infile.Contains("T_TuneZ2_tW-channel"))             sample = Form("ttWchan_%s",      outfileid.Data());
+  else if (infile.Contains("Tbar_TuneZ2_tW-channel"))          sample = Form("tbartWchan_%s",   outfileid.Data());
+  else if (infile.Contains("WW_TuneZ2"))                       sample = Form("ww_%s",           outfileid.Data());
+  else if (infile.Contains("WZ_TuneZ2"))                       sample = Form("wz_%s",           outfileid.Data());
+  else if (infile.Contains("ZZ_TuneZ2"))                       sample = Form("zz_%s",           outfileid.Data());
+  else if (infile.Contains("QCD_Pt-15to20_MuPt5Enriched"))     sample = Form("MuQCD15to20_%s",  outfileid.Data());
+  else if (infile.Contains("QCD_Pt-20to30_MuPt5Enriched"))     sample = Form("MuQCD20to30_%s",  outfileid.Data());
+  else if (infile.Contains("QCD_Pt-30to50_MuPt5Enriched"))     sample = Form("MuQCD30to50_%s",  outfileid.Data());
+  else if (infile.Contains("QCD_Pt-50to80_MuPt5Enriched"))     sample = Form("MuQCD50to80_%s",  outfileid.Data());
+  else if (infile.Contains("QCD_Pt-80to120_MuPt5Enriched"))    sample = Form("MuQCD80to120_%s", outfileid.Data());
+  else if (infile.Contains("SMS-T2tt"))                        sample = Form("T2tt_%s",         outfileid.Data());
+  else if (infile.Contains("WWJetsTo2L2Nu"))                   sample = Form("ww2l2nujets_%s",  outfileid.Data());
   //Data
   //single muon
   else if (infile.Contains("SingleMu_Run2011A-PromptReco-v4_AOD") && infile.Contains("V04-02-33"))  sample =  Form("SingleMu2011A_PromptRecov4V33_%s",  outfileid.Data());
@@ -118,6 +128,20 @@ void processBaby( TString outfileid = "merged_ntuple_35", TString infile = "/nfs
   else if (infile.Contains("DoubleMu_Run2011B-PromptReco-v1_AOD") && infile.Contains("V04-02-34"))  sample =  Form("DoubleMu2011B_PromptRecov1V34_%s",  outfileid.Data());
   else if (infile.Contains("DoubleMu_Run2011A-May10ReReco-v1_AOD") && infile.Contains("V04-02-20")) sample =  Form("DoubleMu2011A-May10ReRecov1V20_%s", outfileid.Data());
   else if (infile.Contains("DoubleMu_Run2011A-05Aug2011-v1_AOD") && infile.Contains("V04-02-30"))   sample =  Form("DoubleMu2011A-05Aug2011v1V30_%s",   outfileid.Data());
+  //electron+muon
+  else if (infile.Contains("MuEG_Run2011A-PromptReco-v4_AOD") && infile.Contains("V04-02-20"))      sample =  Form("MuEG2011A_PromptRecov4V20_%s",  outfileid.Data());
+  else if (infile.Contains("MuEG_Run2011A-PromptReco-v6_AOD") && infile.Contains("V04-02-30"))      sample =  Form("MuEG2011A_PromptRecov6V30_%s",  outfileid.Data());
+  else if (infile.Contains("MuEG_Run2011B-PromptReco-v1_AOD") && infile.Contains("V04-02-30"))      sample =  Form("MuEG2011B_PromptRecov1V30_%s",  outfileid.Data());
+  else if (infile.Contains("MuEG_Run2011B-PromptReco-v1_AOD") && infile.Contains("V04-02-34"))      sample =  Form("MuEG2011B_PromptRecov1V34_%s",  outfileid.Data());
+  else if (infile.Contains("MuEG_Run2011A-May10ReReco-v1_AOD") && infile.Contains("V04-02-20"))     sample =  Form("MuEG2011A-May10ReRecov1V20_%s", outfileid.Data());
+  else if (infile.Contains("MuEG_Run2011A-05Aug2011-v1_AOD") && infile.Contains("V04-02-30"))       sample =  Form("MuEG2011A-05Aug2011v1V30_%s",   outfileid.Data());
+  //dielectron
+  else if (infile.Contains("DoubleElectron_Run2011A-PromptReco-v4_AOD") && infile.Contains("V04-02-20"))  sample =  Form("DoubleElectron2011A_PromptRecov4V20_%s",  outfileid.Data());
+  else if (infile.Contains("DoubleElectron_Run2011A-PromptReco-v6_AOD") && infile.Contains("V04-02-30"))  sample =  Form("DoubleElectron2011A_PromptRecov6V30_%s",  outfileid.Data());
+  else if (infile.Contains("DoubleElectron_Run2011B-PromptReco-v1_AOD") && infile.Contains("V04-02-30"))  sample =  Form("DoubleElectron2011B_PromptRecov1V30_%s",  outfileid.Data());
+  else if (infile.Contains("DoubleElectron_Run2011B-PromptReco-v1_AOD") && infile.Contains("V04-02-34"))  sample =  Form("DoubleElectron2011B_PromptRecov1V34_%s",  outfileid.Data());
+  else if (infile.Contains("DoubleElectron_Run2011A-May10ReReco-v1_AOD") && infile.Contains("V04-02-20")) sample =  Form("DoubleElectron2011A-May10ReRecov1V20_%s", outfileid.Data());
+  else if (infile.Contains("DoubleElectron_Run2011A-05Aug2011-v1_AOD") && infile.Contains("V04-02-30"))   sample =  Form("DoubleElectron2011A-05Aug2011v1V30_%s",   outfileid.Data());
   //otherwise
   else sample = Form("boiade_%s", outfileid.Data());
   cout<<"sample is "<<sample<<endl;
