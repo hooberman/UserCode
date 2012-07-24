3b76268d4f0bdb43491aa098772a04b33a632f20
benhoob <>
2012-07-24 16:13:18 +0000

    adjust CMS label

--- ZMet2011/scripts/exclusionPlots/makePlots.C
+++ ZMet2011/scripts/exclusionPlots/makePlots.C
@@ -80,15 +80,15 @@ void cmsPrelim(double intLumi, bool prelim)
         TLatex latex;
         latex.SetNDC();
         latex.SetTextFont(62);
-        if(prelim) latex.SetTextSize(0.04);
+        if(prelim) latex.SetTextSize(0.035);
         else       latex.SetTextSize(0.045);
 
         latex.SetTextAlign(11); // align left
-        if(prelim) latex.DrawLatex(0.13,0.92,"CMS Preliminary");
-        else       latex.DrawLatex(0.13,0.92,"CMS");
+        if(prelim) latex.DrawLatex(0.17,0.92,"CMS Preliminary");
+        else       latex.DrawLatex(0.17,0.92,"CMS");
 
         latex.SetTextAlign(31); // align right
-        latex.DrawLatex(0.89, 0.92, Form("#sqrt{s} = 7 TeV, L_{int} = %4.2f fb^{-1}", intLumi));
+        latex.DrawLatex(0.85, 0.92, Form("#sqrt{s} = 7 TeV, L_{int} = %4.2f fb^{-1}", intLumi));
 }
 
 void makePlots(){
