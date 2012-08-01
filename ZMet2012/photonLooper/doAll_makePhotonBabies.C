af0a304d97d3ac394961cc168fb02678d9afc126
benhoob <>
2012-08-01 21:54:53 +0000

    run on DoubleElectron

--- ZMet2012/photonLooper/doAll_makePhotonBabies.C
+++ ZMet2012/photonLooper/doAll_makePhotonBabies.C
@@ -4,5 +4,5 @@
   
   //--photon+jets---
   runMakePhotonBabies("DoubleElectron"        , true  );   // DoubleElectron data
-  runMakePhotonBabies("Photon"                , true  );   // Photon data
+  //runMakePhotonBabies("Photon"                , true  );   // Photon data
 }
