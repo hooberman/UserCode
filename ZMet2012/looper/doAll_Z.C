170bf0133143027e6f5ca22dcc727d88c4971ec1
benhoob <>
2012-08-27 14:28:57 +0000

    update samples

--- ZMet2012/looper/doAll_Z.C
+++ ZMet2012/looper/doAll_Z.C
@@ -4,28 +4,34 @@
 
   gROOT->ProcessLine(".L runZLooper.C+");
 
-  // runZLooper("ttbar_massiveb"          , false );  
-  // runZLooper("ww"                      , false );  
-  // runZLooper("t"                       , false );  
-  // runZLooper("wz"                      , false );  
-  // runZLooper("zz"                      , false );  
-  // runZLooper("ttbar"                   , false );  
-  // runZLooper("dataskim2010"            , true  );
-  // runZLooper("zjets"                   , false );  
+  runZLooper("dataskim2010"            , true  );
+  runZLooper("data2012cv2"             , true  );
+  runZLooper("ttbar_massiveb"          , false );  
+  runZLooper("ww"                      , false );  
+  runZLooper("t"                       , false );  
+  runZLooper("wz"                      , false );  
+  runZLooper("zz"                      , false );  
+  runZLooper("ttbar"                   , false );  
+  runZLooper("zjets"                   , false );  
+  runZLooper("zjets_10to50"            , false );  
 
+  // runZLooper("data"                    , true  );
+  // runZLooper("dataskim"                , true  );
+  // runZLooper("data2012c"               , true  );
+  // runZLooper("wzsms"                   , false );  
+  // runZLooper("gmsb"                    , false );  
+  // runZLooper("testfilter_newJEC"       , true  );  
+
+  //--------------------------------------------------
   // samples for sync exercise
-  runZLooper("RelValZEE"                  , false );  
-  runZLooper("RelValZMM"                  , false );
-  runZLooper("MuEG_199752"                , true  );
-  runZLooper("DoubleElectron_199752"      , true  );
-  runZLooper("DoubleMu_199752"            , true  );
+  //--------------------------------------------------
 
-  // runZLooper("data"                  , true  );
-  // runZLooper("dataskim"              , true  );
-  // runZLooper("data2012c"             , true  );
-  // runZLooper("wzsms"                 , false );  
-  // runZLooper("gmsb"                  , false );  
-  // runZLooper("testfilter_newJEC"     , true  );  
+  // runZLooper("RelValZEE"                  , false );  
+  // runZLooper("RelValZMM"                  , false );
+  // runZLooper("MuEG_199752"                , true  );
+  // runZLooper("DoubleElectron_199752"      , true  );
+  // runZLooper("DoubleMu_199752"            , true  );
+
 
 }
 
