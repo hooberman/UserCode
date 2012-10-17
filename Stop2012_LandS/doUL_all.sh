23d1a1d840ef7467582fa453974fe775f06f8acf
benhoob <>
2012-10-17 15:32:10 +0000

    initial commit

---
+++ Stop2012_LandS/doUL_all.sh
@@ -0,0 +1,48 @@
+../test/lands.exe -d SRA_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff00
+../test/lands.exe -d SRB_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff00
+../test/lands.exe -d SRC_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff00
+../test/lands.exe -d SRD_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff00
+../test/lands.exe -d SRE_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff00
+../test/lands.exe -d SRF_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff00
+../test/lands.exe -d SRG_eff00 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff00
+
+../test/lands.exe -d SRA_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff10
+../test/lands.exe -d SRB_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff10
+../test/lands.exe -d SRC_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff10
+../test/lands.exe -d SRD_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff10
+../test/lands.exe -d SRE_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff10
+../test/lands.exe -d SRF_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff10
+../test/lands.exe -d SRG_eff10 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff10
+
+../test/lands.exe -d SRA_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff20
+../test/lands.exe -d SRB_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff20
+../test/lands.exe -d SRC_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff20
+../test/lands.exe -d SRD_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff20
+../test/lands.exe -d SRE_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff20
+../test/lands.exe -d SRF_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff20
+../test/lands.exe -d SRG_eff20 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff20
+
+../test/lands.exe -d SRA_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff30
+../test/lands.exe -d SRB_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff30
+../test/lands.exe -d SRC_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff30
+../test/lands.exe -d SRD_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff30
+../test/lands.exe -d SRE_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff30
+../test/lands.exe -d SRF_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff30
+../test/lands.exe -d SRG_eff30 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff30
+
+../test/lands.exe -d SRA_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff40
+../test/lands.exe -d SRB_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff40
+../test/lands.exe -d SRC_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff40
+../test/lands.exe -d SRD_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff40
+../test/lands.exe -d SRE_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff40
+../test/lands.exe -d SRF_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff40
+../test/lands.exe -d SRG_eff40 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff40
+
+../test/lands.exe -d SRA_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRA_eff50
+../test/lands.exe -d SRB_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRB_eff50
+../test/lands.exe -d SRC_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRC_eff50
+../test/lands.exe -d SRD_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRD_eff50
+../test/lands.exe -d SRE_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRE_eff50
+../test/lands.exe -d SRF_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRF_eff50
+../test/lands.exe -d SRG_eff50 -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n SRG_eff50
+
