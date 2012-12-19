651b1306cc1965820f1a2a94e92194111bb7724c
benhoob <>
2012-12-19 14:04:40 +0000

    update to 19.3/fb

--- ZMet2012/photonLooper/doAll_photonTemplates.C
+++ ZMet2012/photonLooper/doAll_photonTemplates.C
@@ -5,6 +5,7 @@
   gSystem->Load("../../MiniFWLite/libMiniFWLite.so");
   
   //runPhotonTemplates("V00-00-11","Photon");
-  runPhotonTemplates("V00-01-00","DoubleElectron");
+  //runPhotonTemplates("V00-01-00","DoubleElectron");
+  runPhotonTemplates("V00-02-00","data_53X_2012ALL");
   
 }
