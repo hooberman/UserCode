c728f571bba034e091e3686496c829b7eea0405e
benhoob <>
2013-01-23 18:59:06 +0000

    run all samples

--- ZMet2012/photonLooper/doAll_makePhotonBabies.C
+++ ZMet2012/photonLooper/doAll_makePhotonBabies.C
@@ -3,9 +3,9 @@
   gROOT->ProcessLine(".L runMakePhotonBabies.C+");
 
   runMakePhotonBabies("data_53X_2012A"             , true  );  
-  // runMakePhotonBabies("data_53X_2012B"             , true  );  
-  // runMakePhotonBabies("data_53X_2012C"             , true  );  
-  // runMakePhotonBabies("data_53X_2012D"             , true  );  
+  runMakePhotonBabies("data_53X_2012B"             , true  );  
+  runMakePhotonBabies("data_53X_2012C"             , true  );  
+  runMakePhotonBabies("data_53X_2012D"             , true  );  
   
   //--photon+jets---
   //runMakePhotonBabies("DoubleElectron"          , true  );   // DoubleElectron data
