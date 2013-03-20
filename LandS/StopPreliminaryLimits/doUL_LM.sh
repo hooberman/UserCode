640d78bfc01ae0a68ac8991a9f4e969dfb836423
benhoob <>
2013-03-20 13:53:29 +0000

    initial commit

---
+++ LandS/StopPreliminaryLimits/doUL_LM.sh
@@ -0,0 +1,4 @@
+../test/lands.exe -d LM150.txt -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n LM150
+../test/lands.exe -d LM200.txt -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n LM200
+../test/lands.exe -d LM250.txt -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n LM250
+../test/lands.exe -d LM300.txt -M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000 --seed 1234 -n LM300
